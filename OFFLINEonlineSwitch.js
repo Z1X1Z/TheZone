a120c22a042f7ff8ceded72c610ed42cab37483c
Z1X1Z <liveonnoevil@protonmail.ch>
2021-07-20 03:52:16 -0400

    Reuploading all files plus some for offline

---
+++ OFFLINEonlineSwitch.js
@@ -0,0 +1,12 @@
+let online = true
+
+if (online){
+    window.three = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r123/three.min.js"
+    window.threeThrone = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r111/three.min.js"
+}
+else{
+    window.three = "three.min.js"
+    window.threeThrone = "three.min.js"
+}
+
+    
