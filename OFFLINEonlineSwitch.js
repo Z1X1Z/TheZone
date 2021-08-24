c613937a27640419620194fdcb68c4de0f83416c
Z1X1Z <liveonnoevil@protonmail.ch>
2021-08-24 10:49:06 -0400

    should be working autonomously now
    should still use indexOFFLINE

--- OFFLINEonlineSwitch.js
+++ OFFLINEonlineSwitch.js
@@ -2,12 +2,10 @@ if (window.location.protocol.match(/https/i)||window.location.href.match(/zonex.
 {
     window.three = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r123/three.min.js"
     window.threeThrone = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r111/three.min.js"
-    console.log("online")
 }
 else{
     window.three = "three.min.js"
     window.threeThrone = "three.min.js"
-    console.log("offline")
 }
 
     
