890037ff1739cca438f31344d938c802e26801a4
Z1X1Z <liveonnoevil@protonmail.ch>
2021-08-24 13:35:38 -0400

    indexOFFLINE should now be done

--- OFFLINEonlineSwitch.js
+++ OFFLINEonlineSwitch.js
@@ -2,10 +2,12 @@ if (window.location.protocol.match(/https/i)||window.location.href.match(/zonex.
 {
     window.three = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r123/three.min.js"
     window.threeThrone = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r111/three.min.js"
+    window.online=true;
 }
 else{
     window.three = "three.min.js"
     window.threeThrone = "three.min.js"
+    window.online=false;
 }
 
     
