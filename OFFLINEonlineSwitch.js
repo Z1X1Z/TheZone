a8332ab158831ae8fe2c44865ca6cce0725c40f8
Z1X1Z <liveonnoevil@protonmail.ch>
2022-02-03 14:01:38 -0500

    Add files via upload

--- OFFLINEonlineSwitch.js
+++ OFFLINEonlineSwitch.js
@@ -1,10 +1,12 @@
 if (window.location.protocol.match(/http/i)||window.location.href.match(/zonex.space/i))
 {
+    window.threeSonicStarship = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r127/three.min.js"
     window.three = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r123/three.min.js"
     window.threeThrone = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r111/three.min.js"
     window.online=true;
 }
 else{
+    window.threeSonicStarship = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r127/three.min.js"
     window.three = "three.min.js"
     window.threeThrone = "three.min.js"
     window.online=false;
