7749c2ec1c6336ef958cae3ff80af4a0b23e7223
Z1X1Z <liveonnoevil@protonmail.ch>
2022-02-06 10:47:45 -0500

    shifted octave wrap to green, multitouch

--- starshipMod.js
+++ starshipMod.js
@@ -144,7 +144,7 @@ window.addEventListener('keyup', function(event) {
 
     }, false);
             
-            let correction = 14.5 ;
+            let correction = 11.5 ;
             let sound = new Wad({source : 'square'});
             let sound2 = new Wad({source : 'square'});
             let initialAngleSound;
@@ -154,25 +154,27 @@ window.addEventListener('keyup', function(event) {
                let y = e.clientY-window.innerHeight/2.;
                 let x = e.clientX- window.innerWidth/2.;
                    let volume= -Math.sqrt(y*y+x*x)/(Math.max(window.innerHeight,window.innerWidth)/2.);
-                    initialAngleSound = Math.atan2(y,x);
-                    let frequency = Math.pow(2.,((initialAngleSound)/pi/2*12+correction)/12.)*440.;
+                    initialAngleSound = (Math.atan2(y,x)+pi/2.+4*pi)%(2*pi);
+                                         console.log(initialAngleSound)
+                    let frequency = Math.pow(2.,((initialAngleSound)/pi/2*12+correction)/12.)*220.;
                                              sound.pitch=frequency;
                                              sound2.pitch=frequency*2;
-                    sound.setVolume(volume*initialAngleSound/(pi*2));
-                    sound2.setVolume(volume*(1.-initialAngleSound/(pi*2)));
+                    sound.setVolume(0.);
+                    sound2.setVolume(volume);
 
-                    sound.play({env:{attack: .1, release:.02,hold:-1}});
-                    sound2.play({env:{attack: .1, release:.02,hold:-1}});
+                    sound.play({env:{attack: .1, release:.02,hold:-1},label : touchNumber});
+                    sound2.play({env:{attack: .1, release:.02,hold:-1},label : touchNumber});
             }
-                                             
+                                         let    touchNumber=0.;
         function followSound(e){
 
             let y = e.clientY-window.innerHeight/2.;
             let x = e.clientX- window.innerWidth/2.;
             let volume= -Math.sqrt(y*y+x*x)/(Math.max(window.innerHeight,window.innerWidth)/2.);
             let angleSound = Math.atan2(y,x);
-            angleSound=(angleSound-initialAngleSound+4*pi)%(2*pi)+initialAngleSound;
-            let frequency = Math.pow(2.,((angleSound)/pi/2*12+correction)/12.)*440.;
+            angleSound=(angleSound-initialAngleSound+pi/2.+4.*pi)%(2*pi)+initialAngleSound;
+            console.log(angleSound)
+            let frequency = Math.pow(2.,((angleSound)/pi/2*12+correction)/12.)*220.;
             sound.setPitch(frequency);
             sound2.setPitch(frequency*2);
             sound.setVolume(volume*(((angleSound-initialAngleSound))/(2.*pi)));
@@ -180,11 +182,19 @@ window.addEventListener('keyup', function(event) {
         }
 
         if (mobile){
-              container.addEventListener('touchstart', function(e) {startSound(e.touches[0]);}, false);
+                container.addEventListener('touchstart', function(e)
+                                           {
+                    for(var o=0; o<e.touches.length; o++)
+                    {touchNumber=o;startSound(e.touches[o]);}
+                    
+                }, false);
               container.addEventListener('touchmove', function(e) {followSound(e.touches[0]);}, false);
-              container.addEventListener('touchend', function(e){ sound.stop();sound2.stop()}, false);
-              container.addEventListener('touchcancel', function(e){ sound.stop();sound2.stop()}, false);
-            }
+              container.addEventListener('touchend', function(e){for(var o=0; o<e.touches.length; o++)
+                  
+              {touchNumber=o; sound.stop({label : touchNumber});sound2.stop({label : touchNumber});}}, false);
+                
+                container.addEventListener('touchcancel', function(e){for(var o=0; o<e.touches.length; o++)
+                {touchNumber=o; sound.stop({label : touchNumber});sound2.stop({label : touchNumber});}}, false);            }
         else{
              container.addEventListener('mousedown', startSound, false);
              container.addEventListener('mousemove', followSound, false);
@@ -196,7 +206,7 @@ window.addEventListener('keyup', function(event) {
 
             
 var zoomOutEngage=false;
-let pi = 3.14159;
+let pi = Math.PI;
 let inputData;
 let dataArray;
 let bufferSize = fftSize;
