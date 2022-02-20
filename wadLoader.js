b311a5d12db2ec169ef5073018adeb2ad8e26f94
Z1X1Z <liveonnoevil@protonmail.ch>
2022-02-20 07:37:31 -0500

    Add files via upload

--- wadLoader.js
+++ wadLoader.js
@@ -48,7 +48,7 @@ function startSound(e){
     sound.stop();sound2.stop()
     
     let correlationForText=0;
-    if(textON)correlationForText=textOUT.offsetHeight;
+    if(textON)correlationForText=document.getElementById("textOUT").offsetHeight;
     if(mobile)correlationForText+=document.getElementById("hotkeys").offsetHeight;
    let y = e.clientY-(window.innerHeight+correlationForText)/2.;
     let x = e.clientX- window.innerWidth/2.;
@@ -67,7 +67,7 @@ function startSound(e){
 function followSound(e){
             
 let correlationForText=0;
-if(textON)correlationForText=textOUT.offsetHeight;
+if(textON)correlationForText=document.getElementById("textOUT").offsetHeight;
 if(mobile)correlationForText+=document.getElementById("hotkeys").offsetHeight;
 let y = e.clientY-(window.innerHeight+correlationForText)/2.;
 let x = e.clientX-window.innerWidth/2.;
