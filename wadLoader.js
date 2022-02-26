93c43c4466bee84a1c6f727f29cd89a62a7b15cd
Z1X1Z <liveonnoevil@protonmail.ch>
2022-02-25 19:51:39 -0500

    Add files via upload

--- wadLoader.js
+++ wadLoader.js
@@ -41,7 +41,7 @@ function initialize(){
     sound =  new Wad({source : 'sine'});//, tuna   : hyperdriveTUNA});
      sound2 = new Wad({source : 'sine'});//, tuna   : hyperdriveTUNA});
 }
-if(window.location.hash!="#")loadScript(wadLoader,initialize);
+if(window.location.hash!="#m144073")loadScript(wadLoader,initialize);
 else initialize();
 let initialAngleSound;
 function startSound(e){
