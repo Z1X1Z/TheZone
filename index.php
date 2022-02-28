9f3ee75d4a88987360dbdec6799636ad63f0ba02
Z1X1Z <liveonnoevil@protonmail.ch>
2022-02-28 09:35:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php header('Cache-Control: max-age=86400');header('Cache-Control: public header');( 'Location: /index.html' ) ;  ?>
+<?php header('Cache-Control: max-age=86400');header('Cache-Control: public header');header( 'Location: /index.html' ) ;  ?>
