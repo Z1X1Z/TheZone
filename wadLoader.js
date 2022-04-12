cc8d1a73c56a27aa5dd56d2865d02a19e48bfec4
Z1X1Z <liveonnoevil@protonmail.ch>
2022-04-12 12:20:49 -0400

    multitouch

--- wadLoader.js
+++ wadLoader.js
@@ -1,3 +1,4 @@
+let touchNumber=new Map();
 function loadScript(url, callback)
 {
     // Adding the script tag to the head as suggested before
@@ -36,12 +37,12 @@ Overdrive:{
     }
 };
 let correction = 11.5 ;
-let sound;
-let sound2;
+let sound=Array(10);
+let sound2=Array(10);
 function initialize(){
-    
     sound =  new Wad({source : 'sine'});//, tuna   : hyperdriveTUNA});
      sound2 = new Wad({source : 'sine'});//, tuna   : hyperdriveTUNA});
+    
 }
 
 var cdnSwitch="wad.min.js";
@@ -49,7 +50,7 @@ if ( window.sessionStorage.getItem("alreadyReset")=="t"){if (window.online)cdnSw
 loadScript(cdnSwitch,initialize);
 }
 
-let initialAngleSound;
+let initialAngleSound = Array(10);
 function startSound(e){
     sound.stop();sound2.stop()
     
@@ -57,16 +58,16 @@ function startSound(e){
     
    let y = e.clientY-(window.innerHeight+correlationForText)/2.;
     let x = e.clientX- window.innerWidth/2.;
-       let volume= -Math.sqrt(y*y+x*x)/(Math.max(window.innerHeight+correlationForText,window.innerWidth)/2.)*10.;
-        initialAngleSound = (Math.atan2(y,x)+pi/2.+4*pi)%(2*pi);
-        let frequency = Math.pow(2.,((initialAngleSound)/pi/2*12+correction)/12.)*220.;
-                                 sound.pitch=frequency;
-                                 sound2.pitch=frequency*2;
-        sound.setVolume(0.);
-        sound2.setVolume(volume);
-
-        sound.play({env:{attack: .2, release:.1,hold:-1}});
-        sound2.play({env:{attack: .2, release:.1,hold:-1}});
+       let volume= -Math.sqrt(y*y+x*x)/(Math.max(window.innerHeight+correlationForText,window.innerWidth)/2.);
+        initialAngleSound[touchNumber.get(e.identifier)] = (Math.atan2(y,x)+pi/2.+4*pi)%(2*pi);
+        let frequency = Math.pow(2.,((initialAngleSound[touchNumber.get(e.identifier)])/pi/2*12+correction)/12.)*220.;
+                                 //console.log(frequency)
+                                 //sound.pitch=frequency;
+                                 //sound2.pitch=frequency*2.;
+                                 //sound.volume=0.;
+                                 //sound2.volume=volume;
+                                 sound.play({env:{attack: .1, release:.02,hold:-1},pitch:frequency,volume:0,label:touchNumber.get(e.identifier)});
+                                 sound2.play({env:{attack: .1, release:.02,hold:-1},pitch:frequency*2.,volume:volume,label:touchNumber.get(e.identifier)});
 }
                                  
 function followSound(e){
@@ -74,29 +75,43 @@ let correlationForText=document.getElementById("allText").offsetHeight;
 
 let y = e.clientY-(window.innerHeight+correlationForText)/2.;
 let x = e.clientX-window.innerWidth/2.;
-let volume= -Math.sqrt(y*y+x*x)/(Math.max(window.innerHeight+correlationForText,window.innerWidth)/2.)*10.;
+let volume= -Math.sqrt(y*y+x*x)/(Math.max(window.innerHeight+correlationForText,window.innerWidth)/2.);
 let angleSound = Math.atan2(y,x);
-angleSound=(angleSound-initialAngleSound+pi/2.+4.*pi)%(2*pi)+initialAngleSound;
+angleSound=(angleSound-initialAngleSound[touchNumber.get(e.identifier)]+pi/2.+4.*pi)%(2*pi)+initialAngleSound[touchNumber.get(e.identifier)];
 let frequency = Math.pow(2.,((angleSound)/pi/2*12+correction)/12.)*220.;
-sound.setPitch(frequency);
-sound2.setPitch(frequency*2);
-sound.setVolume(volume*(((angleSound-initialAngleSound))/(2.*pi)));
-sound2.setVolume(volume*(1.-((angleSound-initialAngleSound))/(2.*pi)));
+//console.log(frequency)
+                         sound.setPitch({pitch: frequency,label:touchNumber.get(e.identifier)});
+                         sound2.setPitch({pitch:2.*frequency,timeConstant:0.0,label:touchNumber.get(e.identifier)});
+                         sound.setVolume({volume:volume*(((angleSound-initialAngleSound[touchNumber.get(e.identifier)]))/(2.*pi)),timeConstant:0.,label:touchNumber.get(e.identifier)});
+                                                         sound2.setVolume({volume:volume*(1.-((angleSound-initialAngleSound[touchNumber.get(e.identifier)]))/(2.*pi)),timeConstant:0.,label:touchNumber.get(e.identifier)});
+                                                          
+                                                         // sound.play({label:touchNumber,pitch:frequency, volume*(((angleSound-initialAngleSound[touchNumber]))/(2.*pi))},{timeConstant:0.});
+                                                        //  sound2.play({label:touchNumber,pitch:frequency*2.,volume*(1.-((angleSound-initialAngleSound[touchNumber]))/(2.*pi))},{timeConstant:0.});
+                                                          
+
 }
 let c = document.getElementById( 'container' );
 if (navigator.userAgent.toLowerCase().match(/mobile/i)||(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)){
-  c.addEventListener('touchstart', function(e) {e.preventDefault(); e.stopImmediatePropagation();
-      console.log(e.changedTouches[0].identifier)
- startSound(e.touches[0]);}, false);
-  c.addEventListener('touchmove', function(e) {
-      console.log(e.changedTouches[0].identifier)
+    container.addEventListener('touchstart', function(e)
+                               {
+        for(var o=0; o<e.touches.length; o++)
+        {touchNumber.set(e.touches[o].identifier,o);startSound(e.touches[o]);}
+        
+    }, false);
+  container.addEventListener('touchmove', function(e) {
+      for(var o=0; o<e.touches.length; o++)
+          touchNumber.set(e.touches[o].identifier,o); followSound(e.changedTouches[o]);}, false);
+  container.addEventListener('touchend', function(e){
+      for(var o=0; o<e.changedTouches.length; o++)
+        {sound.stop(String(e.changedTouches[o].identifier));
+          sound2.stop(String(e.changedTouches[o].identifier));}
+      
+  }
+                             , false);
 
-      e.preventDefault();e.stopImmediatePropagation();
-                                                followSound(e.touches[0]);}, false);
-  c.addEventListener('touchend', function(e){ e.preventDefault();e.stopImmediatePropagation()
-                                            sound.stop();sound2.stop()}, false);
-  c.addEventListener('touchcancel', function(e){ e.preventDefault();e.stopImmediatePropagation()
-                                            ysound.stop();sound2.stop()}, false);
+    container.addEventListener('touchcancel', function(e){for(var o=0; o<e.changedTouches.length; o++)
+    {sound.stop(String(e.changedTouches[o].identifier));sound2.stop(String(e.changedTouches[o].identifier));}}, false);
+
 }
 else{
  c.addEventListener('mousedown', startSound, false);
