7e8204943eea3e06544d0ab19df54fa2276198de
Z1X1Z <liveonnoevil@protonmail.ch>
2022-08-15 11:39:20 -0400

    Add files via upload

--- wadLoader.js
+++ wadLoader.js
@@ -126,8 +126,10 @@ let c = document.getElementById( 'container' );
 if (navigator.userAgent.toLowerCase().match(/mobile/i)||(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)){
     container.addEventListener('touchstart', function(e)
                                {
+        e.preventDefault(); e.stopImmediatePropagation();
+
         let b = document.getElementsByClassName(dropdown);
-        for (var u = 0; u<b.length; u++)b.blur();
+        for (var u = 0; u<b.length; u++)b[u].blur();
         for(var o=0; o<e.changedTouches.length; o++)
         {
             touchNumber.set(e.changedTouches[o].identifier,cycle);
@@ -148,6 +150,8 @@ if (navigator.userAgent.toLowerCase().match(/mobile/i)||(navigator.platform === 
   container.addEventListener('touchend', function(e){
       window.pointerZoom=false;
       if(!window.touchMode){
+          e.preventDefault(); e.stopImmediatePropagation();
+
       for(var o=0; o<e.changedTouches.length; o++)
         {sound[touchNumber.get(e.changedTouches[o].identifier)].stop();sound2[touchNumber.get(e.changedTouches[o].identifier)].stop();}
       }}
