403aa7afc8a64bdd93af0ab36aec78eadd59a487
Z1X1Z <hallelujah.hephzibah@gmail.com>
2023-02-10 11:53:04 -0500

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -1,7 +1,45 @@
+
+
+let touchOnlyMode = false;
+let micOn = false;
+let audioX;
+
+let analyser;
+let source;
+let dataArray;
+
+async function startMic() {
+  //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+    navigator.mediaDevices.getUserMedia({
+    audio: true,
+    autoGainControl: false,
+    echoCancellation: false,
+    noiseSuppression: false//https://stackoverflow.com/questions/71978189/lag-when-playing-mic-audio-directly-to-output-using-web-audio-api
+    })
+  .then((stream) => {
+    /* use the stream */
+      audioX = new AudioContext();
+      analyser = audioX.createAnalyser();
+      source = audioX.createMediaStreamSource( stream );
+      source.connect(analyser);
+      analyser.fftSize = fftSize;
+      dataArray = new Uint8Array( bufferSize );
+      //init();
+  }).catch((err) => {// engage touch only mode
+                    console.log("Touch only mode!")
+                    touchOnlyMode=true;
+                    window.touchMode = true;
+                    })
+  .finally((err) => {
+    micOn = true;
+      init();
+
+  });
+}
+
 function stallTillTHREE(){//this is a lurker. it waits for the three.js loader to resolve to a loaded library, then initializes the game.
 
-    if(typeof THREE=="object"&& document.visibilityState=="visible"&&
-       (micOn||location.hash.includes("t"))){
+    if(typeof THREE=="object"&& document.visibilityState=="visible"){
         document.getElementById( "background_wrap").style = "position: unset;";//turn off splash!
 
                 if(location.hash.includes("t"))
@@ -9,7 +47,7 @@ function stallTillTHREE(){//this is a lurker. it waits for the three.js loader t
                 touchOnlyMode=true;
                   pointerZoom=true;
               }
-         init();
+        startMic();
             
     }else setTimeout(stallTillTHREE,10);}//setTimeout waits for 10ms then runs init()
 stallTillTHREE();
