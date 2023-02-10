1e3c998dc90124c01120fffe39df6cbe50d4ab67
Z1X1Z <hallelujah.hephzibah@gmail.com>
2023-02-10 13:31:04 -0500

    trying this again for fullscreen

--- starshipMod.js
+++ starshipMod.js
@@ -1,15 +1,53 @@
+
+
+let touchOnlyMode = false;
+let micOn = false;
+let audioX;
+
+let analyser;
+let source;
+let dataArray;
+
+async function startMic() {
+  //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+    navigator.mediaDevices.getUserMedia({
+    audio: true,
+    autoGainControl: false,
+    echoCancellation: false,
+    noiseSuppression: false//https://stackoverflow.com/questions/71978189/lag-when-playing-mic-audio-directly-to-output-using-web-audio-api
+    })
+  .then((stream) => {
+    /* use the stream */
+      audioX = new AudioContext();
+      analyser = audioX.createAnalyser();
+      source = audioX.createMediaStreamSource( stream );
+      source.connect(analyser);
+      analyser.fftSize = fftSize;
+      dataArray = new Uint8Array( bufferSize );
+      //init();
+  }).catch((err) => {// engage touch only mode
+                    console.log("Touch only mode!")
+                    touchOnlyMode=true;
+                    window.touchMode = true;
+                    })
+  .finally((err) => {
+      document.getElementById( "background_wrap").style = "position: unset;";//turn off splash!
+    micOn = true;
+      init();
+
+  });
+}
+
 function stallTillTHREE(){//this is a lurker. it waits for the three.js loader to resolve to a loaded library, then initializes the game.
 
-    if(typeof THREE=="object"&& document.visibilityState=="visible"&&
-       (micOn||location.hash.includes("t"))){
-        document.getElementById( "background_wrap").style = "position: unset;";//turn off splash!
+    if(typeof THREE=="object"&& document.visibilityState=="visible"){
 
                 if(location.hash.includes("t"))
               {
                 touchOnlyMode=true;
                   pointerZoom=true;
               }
-         init();
+        startMic();
             
     }else setTimeout(stallTillTHREE,10);}//setTimeout waits for 10ms then runs init()
 stallTillTHREE();
