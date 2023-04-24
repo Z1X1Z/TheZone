63328273958fa59145ca09e381ad7d048050594a
Z1X1Z <hallelujah.hephzibah@gmail.com>
2023-04-24 12:30:21 -0400

    Background off didn't work
    trying more off

--- starshipMod.js
+++ starshipMod.js
@@ -2,7 +2,7 @@
 function stallTillTHREELoaded(){//this is a lurker. it waits for the three.js loader to resolve to a loaded library, then initializes the game.
     if(typeof THREE=="object" && document.visibilityState=="visible"
        &&(window.micOn||location.hash.includes("t"))){
-        document.getElementById( "background_wrap").style = "background-image: none;";//turn off splash!
+       // document.getElementById( "background_wrap").style = "background-image: none;";//turn off splash!
 
         document.getElementById( "load message").innerHTML = "";//turn off splash!
 
