8967dc02af53081d8968bf5c5d0bc4d4781977e8
Z1X1Z <hallelujah.hephzibah@gmail.com>
2023-06-06 11:43:37 -0400

    si Dios quiere!

--- starshipMod.js
+++ starshipMod.js
@@ -2,7 +2,8 @@ var THREE;
 if(window.useCDN)import("three").then(module => { THREE=module})
 .catch((err) => {
     console.log("Error loading threeJS module;load old Threejs instead");
-    THREE = "https://cdn.jsdelivr.net/gh/Z1X1Z/zonex_jsdelivr/three.min.js"//iOS needs a local module, so we give it this instead
+    document.getElementById("threeJSscript").src="three.js";
+ = "https://cdn.jsdelivr.net/gh/Z1X1Z/zonex_jsdelivr/three.min.js"//iOS needs a local module, so we give it this instead
     
 })
 .finally((err) => {});
