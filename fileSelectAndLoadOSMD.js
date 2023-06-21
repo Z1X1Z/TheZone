3472bef6fa4fde7a3a08058b5c663b0c260b3ef6
Z1X1Z <hallelujah.hephzibah@gmail.com>
2023-06-21 10:37:40 -0400

    Add files via upload

--- fileSelectAndLoadOSMD.js
+++ fileSelectAndLoadOSMD.js
@@ -52,7 +52,7 @@ function loadScore(e) {
         drawMeasureNumbers:true, drawMeasureNumbersOnlyAtSystemStart:true, drawTimeSignatures:true,
         autoResize: false,
       backend: "png",
-        SkyBottomLineBatchCalculatorBackendType:"Plain",
+    preferredSkyBottomLineBatchCalculatorBackend:0,//0 Plain or 1 Webgl
       drawFromMeasureNumber: 1,
       drawUpToMeasureNumber: 1+Math.floor(window.innerWidth/window.innerHeight*2.)// draw all measures, up to the end of the sample
     });
