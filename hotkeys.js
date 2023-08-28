5344cade17a53848dc439fe5694a55422fce738c
Z1X1Z <hallelujah.hephzibah@gmail.com>
2023-08-28 18:22:22 -0400

    Add files via upload

--- hotkeys.js
+++ hotkeys.js
@@ -55,7 +55,10 @@ function callKey(event){
     var x=null;
     if(!event.shiftKey)x = parseInt(String.fromCharCode( event.keyCode));
 
+
     //meta keys like ctrlKey must be processed first and should have symbol preferably
+    if (document.activeElement.className=="num");//don't take hotkey's while menu number selector engaged
+
     else if(key == "k" && event.ctrlKey)
     {
         window.timeRESET= window.TIMESTAMP;
@@ -99,23 +102,22 @@ function callKey(event){
         }
     } the bass staff doesn't include the lyrics so it's not included
      */
-    else if (document.activeElement.className!="num")
+
+    else if (x>0&&x<=4&& document.activeElement.className!="num")
+    {rez = window.devicePixelRatio/x; renderer.setPixelRatio( rez);}
+    else if (key=="+"){rez /=1.1; renderer.setPixelRatio( rez);}
+    else if (key=="_"){rez *=1.1; renderer.setPixelRatio( rez);}
+
+    else if (x==0)
+    {window.movementRate=pixelShaderSize/4.; uniforms[ "rate" ].value=1./window.movementRate; }
+    else if(x == 7&&!event.shiftKey)uniforms.musicAngelMan.value=(uniforms.musicAngelMan.value+1)%3;
+    else if(x == 8&&!event.shiftKey)
     {
-        if (x>0&&x<=4&& )
-        {rez = window.devicePixelRatio/x; renderer.setPixelRatio( rez);}
-        else if (key=="+"){rez /=1.1; renderer.setPixelRatio( rez);}
-        else if (key=="_"){rez *=1.1; renderer.setPixelRatio( rez);}
-        
-        else if (x==0)
-        {window.movementRate=pixelShaderSize/4.; uniforms[ "rate" ].value=1./window.movementRate; }
-        else if(x == 7&&!event.shiftKey)uniforms.musicAngelMan.value=(uniforms.musicAngelMan.value+1)%3;
-        else if(x == 8&&!event.shiftKey)
-        {
-            if(uniforms.refactorCores.value==2)uniforms.refactorCores.value=1;
-            else if(uniforms.refactorCores.value==1)uniforms.refactorCores.value=0;
-            else uniforms.refactorCores.value=2;
-        }
+        if(uniforms.refactorCores.value==2)uniforms.refactorCores.value=1;
+        else if(uniforms.refactorCores.value==1)uniforms.refactorCores.value=0;
+        else uniforms.refactorCores.value=2;
     }
+    
     else if (key=="À"||key=="`")
     {rez=window.devicePixelRatio*2.;renderer.setPixelRatio( rez);}
     else if (key=="m") uniforms[ "wheel" ].value = !uniforms[ "wheel" ].value;
