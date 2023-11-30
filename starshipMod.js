612d8b48a64763cd250eae2119f208e2d1c57f73
Z1X1Z <hallelujah.hephzibah@gmail.com>
2023-11-30 11:25:19 -0500

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -2699,7 +2699,7 @@ function calculatePitch ()
 
 let tolerance; //, confidence;
         if(highORlow==1){
-            tolerance=totalAMP-totalAMP/(fractionOfFrame*(-leaf*gr));
+            tolerance=totalAMP-totalAMP/(fractionOfFrame*(-leaf*-leaf));
         }
 else if (highORlow==2)tolerance = .5;//when I play different notes on harmonica it mostly hears C, this clears up the distinction of the notes
                         
