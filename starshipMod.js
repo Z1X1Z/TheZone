4455c4f14bc2f695f4e13ed4264b8bc57eef1c5a
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-02-13 17:46:45 -0500

    there's no error message on iPad
    yet it's crashing when the sound is played for a while in starstream

--- starshipMod.js
+++ starshipMod.js
@@ -1804,6 +1804,7 @@ if( (!window.touchMode||window.shouldShowStar)&&!window.touchOnlyMode) {
             let loopsToCull = starCount;
             let loopOfCulling =starStreamIndex;
             let shellBoost = 1.5;
+            /*
            if(xyStarParticleArray[loopOfCulling]!=null) while(xyStarParticleArray[(loopOfCulling-1+starCount)%starCount]!=null&&uniforms[ "time" ].value-xyStarParticleArray[loopOfCulling].time>maxToMin*secondsToEdge*shellBoost&&loopsToCull>0)
             {
                // xyStarParticleArray.shift();
@@ -1818,7 +1819,7 @@ if( (!window.touchMode||window.shouldShowStar)&&!window.touchOnlyMode) {
                 loopsToCull--;
 
             }
-            
+            */
             let OUTERSHELL =maxToMin* secondsToEdge;
             let m = xyStarParticleArray[starStreamIndex];
             if(m!=null)
