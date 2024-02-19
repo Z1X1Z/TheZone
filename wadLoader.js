4929606c2f05cdcd4553f5cc88d634590651b123
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-02-19 09:58:18 -0500

    trying

--- wadLoader.js
+++ wadLoader.js
@@ -106,7 +106,7 @@ function startSound(e){
 
     if(window.grabStar){
         initialAngle[id]=Math.atan2(y,x)/Math.PI/2.;
-        window.twist=(window.twist)%24
+        window.twist=(window.twist+24)%24
         initialTwist[id]=window.twist
         lastTwist[id] =0;
 
