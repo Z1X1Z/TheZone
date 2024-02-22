8adec06dd5956194b9b754cfdc4304fc89225d64
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-02-22 01:19:38 -0500

    Add files via upload

--- wadLoader.js
+++ wadLoader.js
@@ -218,7 +218,7 @@ if(window.grabStar)
     for(var t=0; t<maxTouchSoundCount;t++)
     {
         let lastTwistSign=signTwist[t];
-        let twisteR=((twist-initialTwist[t]+permanentInitialTwist[id]+24*100)%24-permanentInitialTwist[id]+24*100)%24+initialTwist[t];
+        let twisteR=((twist+permanentInitialTwist[t]-initialTwist[t]+24*100)%24-permanentInitialTwist[t]+initialTwist[t]+24*100)%24;
        // console.log("aaaa"+permanentInitialTwist[t])
 
         signTwist[t] =Math.sign(twisteR-12.);
