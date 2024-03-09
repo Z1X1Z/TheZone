d829c45a3e01b01ef3efdf2d5ef299ce1d1b446b
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-03-08 21:40:28 -0500

    sunified spokefactorlarge and upspoke
    There seems to be a lot of possible answers.  What I like is what looks like bouquets.

--- bigTreeOfLife.js
+++ bigTreeOfLife.js
@@ -215,9 +215,10 @@ var dstnce = s.length();//"distance" may be reserved keyword
 var coreBooster=0.;
 var metaCoreDriveFactor =(((1.-leaf)**.5/truncator)*truncator)**2./gr;//.324717.... number of places changes appearance
 var spoke_factor =metaCoreDriveFactor*(((-2.*gr-3.*leaf)/truncator)*truncator);//metaCoreDriveFactor*(((-2.*gr-3.*leaf)/truncator)*truncator)
-var grOverLeaf=-((gr/leaf)/truncator)*truncator;//uncertain term
-var spoke_factorLarge = spoke_factor*(((1.+gr)/-leaf)/truncator)*truncator;
-                            var upSpoke=Math.pow(spoke_factor,metaCoreDriveFactor)*1.5;//uncertain term, without dstnce spokeCore is spoke_factor*2. try times spoke_factorLarge
+//var grOverLeaf=-((gr/leaf)/truncator)*truncator;//uncertain term
+var upSpoke= Math.pow(spoke_factor,((-leaf/gr)/truncator)*truncator)*(((1.+gr)/-leaf)/truncator)*truncator;
+var spoke_factorLarge = upSpoke;
+                            //uncertain term, without dstnce spokeCore is spoke_factor*2. try times spoke_factorLarge
 
            var downSpoke=((1./leaf)/truncator)*truncator;//1./(((-leaf)*truncator)/truncator)/4.;
   // var logOfSpoke_Factor=0.;
@@ -348,7 +349,7 @@ s.x=Math.log(Math.abs(s.x))/Math.log(baseN);
                     }
                     else*/
                      {
-                      //  var correctionSpoke =Math.pow(upSpoke/dstnce,grOverLeaf);
+                  //      var correctionSpoke =Math.pow(upSpoke/dstnce,grOverLeaf);
 
                         hyperCoreOUTPUT+=upSpoke;
                         hyperCoreBoosted+=upSpoke;
