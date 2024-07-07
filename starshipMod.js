cf407c81034872d7cf0112add6da6e372ed55637
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-07-06 23:49:52 -0400

    tolerance and leaf/100!

--- starshipMod.js
+++ starshipMod.js
@@ -2847,7 +2847,7 @@ function calculatePitch ()
                        // return Math.abs(inputData[0]-inputData[1])/audioX.sampleRate*4.
 
 let tolerance;//(1024-26)/10000
-if(window.highORlow==1)tolerance=.0214285;
+if(window.highORlow==1)tolerance=-(leaf-1)/100.;
 //.02134356 first solid guess//.0214284 easier reaching notes//n*2,n,n*2*2,n*2*2/2,n*2*2*2,n*2*2*2/2
     else if(window.highORlow==2)tolerance=.49;
 let period;
