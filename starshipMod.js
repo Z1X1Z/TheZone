14aa05b49bb4048b2bb15fc82b0cc3da183d7aaf
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-07-07 16:14:43 -0400

    hiccup on android with last uploads

--- starshipMod.js
+++ starshipMod.js
@@ -2847,7 +2847,7 @@ function calculatePitch ()
                        // return Math.abs(inputData[0]-inputData[1])/audioX.sampleRate*4.
 let tolerance;//(1024-26)/10000
              if(window.highORlow==1){
-                 let tAScaled=totalAMP*fractionOfFrame/bufferSize;
+                 let tAScaled=totalAMP*fractionOfFrame/bufferSize/2.;
                  tolerance=tAScaled**(1.+tAScaled);
              }
                  
