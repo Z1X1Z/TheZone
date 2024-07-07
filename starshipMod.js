0b2efa979db5696a9f25df2fa7b9308b733f8b67
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-07-07 17:16:45 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -2847,8 +2847,8 @@ function calculatePitch ()
                        // return Math.abs(inputData[0]-inputData[1])/audioX.sampleRate*4.
 let tolerance;//(1024-26)/10000
              if(window.highORlow==1){
-                 let tAScaled=totalAMP*fractionOfFrame/bufferSize/2.;
-                 tolerance=tAScaled**(1.+tAScaled);
+                 let tAScaled=(totalAMP*fractionOfFrame/bufferSize);
+                 tolerance=(tAScaled**(1.-tAScaled)+tAScaled**(1.+tAScaled))
              }
                  
 //.02134356(7)  solid guess//.0214284 easier reaching notes//n*2,n,n*2*2,n*2*2/2,n*2*2*2,n*2*2*2/2
