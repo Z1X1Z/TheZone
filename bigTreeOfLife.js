ac6728aa947d5424c2757463e1062cd3cb1f927f
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-07-08 19:05:13 -0400

    Add files via upload

--- bigTreeOfLife.js
+++ bigTreeOfLife.js
@@ -219,7 +219,8 @@ var term=0.;
 var m= new THREE.Vector2(0.,0.);
 
     var truncator=1.;
-    if(lfc!=0.) truncator = Math.log(zoom/lfc);
+    var truncated = false;
+    if(lfc!=0.&&truncated) truncator = Math.log(zoom/lfc);
 //Maendel clover
 if(wheel)m =  pWithoutChiralizer.clone().sub(new THREE.Vector2(coords.y,coords.x).multiplyScalar(2.))//try signs with for fibonacci ring pairing and movement distortion #syyym
 .multiplyScalar(Math.abs(coresIn/crs*2.-1.)).multiplyScalar(mandelCloverFactor);
