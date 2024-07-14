e18a9e70e73473c31472a3e3d50ec0d3b823584b
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-07-14 00:07:09 -0400

    heart adjusts from zoomOutRatchetThreshold

--- hotkeys.js
+++ hotkeys.js
@@ -13,6 +13,7 @@ const secondsToEdge=window.pixelShaderSize/4./pixelShaderToStarshipRATIO;
 window.uniformsInitial = {
 coreDilation:{value:0.},
 fftSize:{value:2048.},sampleRate:{value:44100.}, nyq:{value:1024./44100.},//actually 2/nyquist
+zoomOutRatchetThreshold:{value:1.},
 radialWarp:{value:1.},
     pixelSTARon:{value:true},
 
