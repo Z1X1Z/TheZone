d68aa38d5a66574523d95358d40673161fd4ae09
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-07-20 20:06:21 -0400

    .5005 had high range hiccups
    **(.75-tAScaledPermanent**(.5+tAScaledPermanent))

--- starshipMod.js
+++ starshipMod.js
@@ -2864,9 +2864,11 @@ let tolerance;//(1024-26)/10000
                                                if(tAScaled>0&&isFinite(tAScaled))
                                                    for(var reps=0; reps<1.;reps+=tAScaled)
                                                 { tAScaled=((tAScaled**(1.-tAScaled)+tAScaled**(1.+tAScaledPermanent))/(2.-tAScaledPermanent))
-                                                    tAScaled=tAScaled**((1.-tAScaled)*(1.+tAScaledPermanent))**(.75-tAScaledPermanent**.5005)
+                                                    tAScaled=tAScaled**((1.-tAScaled)*(1.+tAScaledPermanent))**(.75-tAScaledPermanent**(.5+
+                                                                                                                                        tAScaledPermanent))
                                           //          b++
                                                 }
+
                                 // console.log(b)
                                // console.log(tAScaled)
                                                tolerance =tAScaled
