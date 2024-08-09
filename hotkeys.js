f211c99a24ebb595c6dc49171c1b8569b6a5f00e
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-08-09 19:02:04 -0400

    mobile variable was being overset

--- hotkeys.js
+++ hotkeys.js
@@ -451,7 +451,7 @@ function callKey(event){
     else if (event.altKey&&(key=="µ"||key=="m"))
     {if(number!="no number")
         uniforms.twelveGatesMeta.value=number;
-        else if( uniforms.twelveGatesMeta.value>=1.)uniforms.twelveGatesMeta.value=0
+        else if( uniforms.twelveGatesMeta.value==1.)uniforms.twelveGatesMeta.value=0
           else  uniforms.twelveGatesMeta.value=1;
     }
 
@@ -489,7 +489,7 @@ function callKey(event){
 
         let content = document.getElementsByClassName("dropdown-content");
         //iframe redirect from https://stackoverflow.com/questions/28159920/how-to-redirect-page-inside-iframe-to-another-one-but-we-must-stay-in-iframe
-        if(mobile||runningHash
+        if(runningHash
            
            );//dynamic href for iFrame doesn't seem to work on mobile
         else if (number == 8)
