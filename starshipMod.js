a42bd0b1bd6b71442f767baceb066901044a97f1
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-12-26 09:57:18 -0500

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -364,12 +364,14 @@ let pushBackCounter = 0;
 
 
                 
+                if(totalAMP>0.)
+                {
                     lastPitch = pitch;
                     // pitch =   (totalAMP>zoomOutRatchetThreshold)? audioX.sampleRate/calculatePitch():pitch;
                     pitch = audioX.sampleRate/calculatePitch();
                     const notNyquist = Math.abs(pitch-audioX.sampleRate/numberOfBins/2.)>1.;
-                    if(!notNyquist&&totalAMP>0.) pitch = lastPitch;
-                
+                    if(!notNyquist) pitch = lastPitch;
+                }
                 
                 
             if (isFinite(pitch) &&pitch>0&& notNyquist &&pitch!=-1&&totalAMP>zoomOutRatchetThreshold) {
