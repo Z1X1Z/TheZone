07f69a76f6490ccce979fd16a4ba53669fd5ac6f
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-12-26 17:13:37 -0500

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3221,7 +3221,7 @@ let tolerance=0;//(1024-26)/10000
                                                                      else break;
                                                                  }
                                                                  
-                                                                 for(n=-1;n<111;n++)
+                                                                 for(n=0;n<111;n++)
                                                                  {
                                                                      let plusOrMinusPowerSeries = totalAMP**n*Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                      if(plusOrMinusPowerSeries!=0.)tAScaled+=plusOrMinusPowerSeries;
