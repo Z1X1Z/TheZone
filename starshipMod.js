0b6e7e61147e148749377465b24df6d323f3de05
Z1X1Z <hallelujah.hephzibah@gmail.com>
2024-12-26 21:04:11 -0500

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3244,14 +3244,15 @@ let tolerance=0;//(1024-26)/10000
                                                                  
                                                                  // console.log(b)
                                                                  // console.log(tAScaled)
-                                                                 
+                                                                 /*
                                                                  for(n=2;n<6;n++)
                                                                  {
                                                                      let plusOrMinusPowerSeries = (tAScaled)**n*-Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                      if(plusOrMinusPowerSeries!=0.)tolerance+=plusOrMinusPowerSeries;
                                                                      else break;
                                                                  }
-                                                                 //tolerance=tAScaled**2-tAScaled**3
+                                                                  */
+                                                                 tolerance=tAScaled-(tAScaled**2)+tAScaled**3
                                                              }
                                                           else tolerance=0.;
                                                          }
