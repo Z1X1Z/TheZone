e5c587d2429ce235b47e9b2ea9244107489ec9dc
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-01-01 14:33:29 -0500

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -394,9 +394,9 @@ let pushBackCounter = 0;
                                     
                                     
                                     
-                                    
+                                lastNote = note;
+
                                 if(totalAMP>0){
-        lastNote = note;
          note = 12*Math.log(pitch/window.ConcertKey)/Math.log(2.)+49;//https://en.wikipedia.org/wiki/Piano_key_frequencies
          uniforms.note.value=note;
         const t =  (note +twist/2)*flip;
