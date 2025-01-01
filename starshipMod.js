7ca853113797e0cfd5a61524a33170777fce220d
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-01-01 15:36:52 -0500

    full reversion

--- starshipMod.js
+++ starshipMod.js
@@ -390,26 +390,23 @@ let pushBackCounter = 0;
                                     
                                     
                                     
-                                
-                lastNote = note;
-                note = 12*Math.log(pitch/window.ConcertKey)/Math.log(2.)+49;//https://en.wikipedia.org/wiki/Piano_key_frequencies
                                     
+                                    
+                                    
+                                    
+        lastNote = note;
+         note = 12*Math.log(pitch/window.ConcertKey)/Math.log(2.)+49;//https://en.wikipedia.org/wiki/Piano_key_frequencies
+         uniforms.note.value=note;
+                            
+        const t =  (note +twist/2)*flip;
+                            if(isFinite(t))angle = -(t*radialWarp);
+                            let reversableColor=((uniforms.brelued.value*angle/12./((radialWarp>0)?radialWarp:1))*flip+twist/24.*uniforms.brelued.value+1./3.)%1.;
+             //if(uniforms.brelued.value==-1)reversableColor=.25-reversableColor;
+
+                            colorSoundPURE =     new THREE.Color().setHSL(reversableColor,1.,.5);
+                            
             if(on)
             {
-                
-                    
-
-uniforms.note.value=note;
-        
-const t =  (note +twist/2)*flip;
-        if(isFinite(t))angle = -(t*radialWarp);
-        let reversableColor=((uniforms.brelued.value*angle/12./((radialWarp>0)?radialWarp:1))*flip+twist/24.*uniforms.brelued.value+1./3.)%1.;
-//if(uniforms.brelued.value==-1)reversableColor=.25-reversableColor;
-
-        colorSoundPURE =     new THREE.Color().setHSL(reversableColor,1.,.5);
-        
-                 //  if(on) {
-
             const colortone = note/lightingScaleTrail;
             colorSound.setHSL(reversableColor,1.,(colortone<=.875)?((colortone>.125)?colortone:.25):.875);//lighting {note/x} should be 120 but it's out of the vocal range
             //angle-=1/radialWarp;
@@ -448,10 +445,9 @@ const t =  (note +twist/2)*flip;
                  pongRoutine(d_x,d_y);
                             d_x*=flatline;
                             d_y*=flatline
-                   if(on){
-    uniforms.d.value.x=d_x;
-    uniforms.d.value.y=d_y;
-}
+                            uniforms.d.value.x+=d_x;
+                                uniforms.d.value.y+=d_y;
+
                                
                                FEEDBACKuniforms.d.value=new THREE.Vector2(d_x,d_y);
                                FEEDBACKuniformsFlip.d.value=new THREE.Vector2(d_x,d_y);
@@ -733,6 +729,7 @@ uniforms.dotCoord.value = new THREE.Vector2(0.,0.);
 function init() {
              colorSound = new THREE.Color();
                   colorSoundPURE =     new THREE.Color();
+             colorSound = new THREE.Color();
 
             // setFFTdependantSizes();
              //setTrailSize();
@@ -1495,9 +1492,6 @@ function runOSMD (){
         //OSMDUPDATER();
                                 let TouchMicroizer = false;
                function executeTouchRegime(){
-                   
-                                                     uniforms.d.value.x = 0.;
-                                                     uniforms.d.value.y = 0.;
                    
                    let coordinator = pixelShaderSize/2./minimumDimension*movementRate;//pixelShaderSize/2 is the frame size in the shader: "p=vec2(...."
                    if(xTouch==0&&yTouch==0&&!TouchMicroizer)
@@ -3183,6 +3177,8 @@ for(var n = 0; n<targets.length;n++){
                      if (!iOS||(iOS&&dupered)) boot();//generate clover in 64 bit, duper Core, there is a bug after maybe half a day on iOS in bigTree.js (maybe also on safari Mac)
 
                                                        
+                                                                                         uniforms.d.value.x = 0.;
+                                                                                         uniforms.d.value.y = 0.;
                                                                        animateLoopId=                   window.requestAnimationFrame( animate );
                             //  renderer.forceContextLoss ()
                             //  renderer.forceContextRestore ( )
