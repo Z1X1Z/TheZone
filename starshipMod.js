5e13985a349f52deb367dfb205c6e900269edbab
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-01-05 12:36:03 -0500

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -1069,7 +1069,8 @@ function onWindowResize() {
                             bottomOfScreenHeight = correlationForTextY;
                         }
                         else{correlationForTextY=0;bottomOfScreenHeight=0}
-                    if(((((document.getElementById("reader").value==0&&readerSet)||(bibleWithText&&!readerSet)))||mobile)&&window.BibleON==0)
+                    if(((((document.getElementById("reader").value==0&&readerSet)||(bibleWithText)))||mobile)&&window.BibleON==0)
+                        
                     correlationForTextX=document.getElementById("Bible").offsetWidth*.5
                             else correlationForTextX=0.;
                             heightPX=window.innerHeight-correlationForTextY;
