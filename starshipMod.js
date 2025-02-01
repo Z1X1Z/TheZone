cb789132693052f422ef7bf2ce22afdaaf6acab9
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-02-01 12:30:58 -0500

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -1557,7 +1557,7 @@ function runOSMD (){
                                     let dxVolumized =xTouchMicroBuffer
                                     let dyVolumized =yTouchMicroBuffer
 
-                                    if(uniforms[ "volume" ].value>.001)//a specific value from one platform
+                                    if(uniforms[ "volume" ].value>.000001)
                                     {
                                         dxVolumized /=uniforms[ "volume" ].value
                                         dyVolumized /=uniforms[ "volume" ].value
