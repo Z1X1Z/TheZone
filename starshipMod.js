a7e69220205c5851882e36b3658822ea6823998f
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-05-05 22:33:53 -0400

    phone was partially crashing on rail

--- starshipMod.js
+++ starshipMod.js
@@ -1243,12 +1243,12 @@ function zoomRoutine(){
     if(uniforms[ "colorCombo" ].value==16)zoomCone/=1.33333333/2.;
     
     ZR = setZoomRate();
-    if (zoom>=1.)
+    if (zoom>=.99)//could work as one (also below) but my phone was partially crashing on continuum clover zoomout
         zoomOutEngage = false;
     if(!isFinite(ZR))ZR=1;
     if(!zoomOutEngage&&zoomRate>0.){
         if ((zoom>zoomCone && totalAMP>zoomOutRatchetThreshold&&(on&&!window.touchMode))||xTouch+yTouch!=0)zoom *=ZR;
-        else if(uniforms.MetaCored.value||zoom<1.){
+        else if(uniforms.MetaCored.value||zoom<.99){
             zoom /= ZR;
             if(center&&zoom<1.){coordX*=ZR*2./3.;; coordY*=ZR*2./3.;}
         }
