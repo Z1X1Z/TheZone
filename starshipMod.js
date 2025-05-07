3690c968711b6c73309a4de1af316ba9ce23b7e4
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-05-06 20:58:18 -0400

    bigger zoom cage yet

--- starshipMod.js
+++ starshipMod.js
@@ -1218,7 +1218,7 @@ let lastVolume = 1.;
         
         expandedZoomCage=1;
              if (uniforms.Spoker.value)expandedZoomCage*=2.
-                 if (uniforms.continuumClover.value)expandedZoomCage/=1.8
+                 if (uniforms.continuumClover.value)expandedZoomCage/=1.75
 
     else expandedZoomCage = 1.;
              
