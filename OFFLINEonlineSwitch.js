fe1e19b758b1c5a838ee99a0b835c27fb60a105d
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-09 05:10:42 -0400

    worked offline, maybe online too!

--- OFFLINEonlineSwitch.js
+++ OFFLINEonlineSwitch.js
@@ -1,6 +1,6 @@
 if (window.location.protocol.match(/http/i)||window.location.href.match(/zonex.space/i))
 {
-    window.threeSonicStarship = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r127/three.min.js"
+    window.threeSonicStarship = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r178/three.min.js"
     window.three = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r123/three.min.js"
     window.threeThrone = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r111/three.min.js"
     window.online=true;
@@ -8,7 +8,7 @@ if (window.location.protocol.match(/http/i)||window.location.href.match(/zonex.s
 
 }
 else{
-    window.threeSonicStarship = "threer127.min.js"
+    window.threeSonicStarship = "three.core.min.js"//"threer127.min.js"
     window.three = "three.min.js"
     window.threeThrone = "three.min.js"
     window.online=false;
