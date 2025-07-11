6744ca70f97ea68a1c0547070f55d6851b16676c
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-11 11:45:09 -0400

    He flows clean!

--- starshipMod.js
+++ starshipMod.js
@@ -3681,10 +3681,10 @@ let tolerance=0;//(1024-26)/10000
                                                                  // console.log(tAScaled)
                                                         //       let grTimesLeaf =   grPermanent*-leafPermanent
                                                       // let feedbackVal = grTimesLeaf;
-                                                                 if(1==1) for(n=2;n<6            ;n++)//still runs at higher n (2000ish)//seems to work better started at 2
+                                                                 if(1==1) for(n=2;n<100            ;n++)//still runs at higher n (2000ish)//seems to work better started at 2
                                                                  {
                                                   
-                                                                     let plusOrMinusPowerSeries = (tAScaled**(2**(-1./n)//*grTimesLeaf
+                                                                     let plusOrMinusPowerSeries = -(tAScaled**(2**(-1./n)//*grTimesLeaf
                                                                                                                ))*Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     
                                                                      if(plusOrMinusPowerSeries!=0.)tolerance+=plusOrMinusPowerSeries;
