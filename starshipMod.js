432dc7c3e84dce0dfd301fcacec94cd357531a91
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-11 01:43:27 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3669,7 +3669,7 @@ let tolerance=0;//(1024-26)/10000
                                                                      for(var reps=0; reps<1.;reps+=tAScaled)
                                                                      { tAScaled=((tAScaled**(1.-tAScaled)+tAScaled**(1.+tAScaledPermanent))/(2.-tAScaledPermanent))
                                                                          tAScaled=tAScaled**((1.-tAScaled)*(1.+tAScaledPermanent))**(
-                                                                                                                                            .75
+                                                                                                                                            oolp
                                                                                                                                      -(tAScaled)**(.5+tAScaled))
                                                                          //          b++
                                                                      }
