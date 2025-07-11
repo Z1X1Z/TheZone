b53420b4fd25070d4f961c67875388105c1e1349
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-11 07:21:42 -0400

    seemed to like oolp

--- starshipMod.js
+++ starshipMod.js
@@ -3679,10 +3679,10 @@ let tolerance=0;//(1024-26)/10000
                                                                  
                                                                  // console.log(b)
                                                                  // console.log(tAScaled)
-                                                                 
-                                                                if(1==0) for(n=2;n<2000;n++)//still runs at higher n (2000ish)
+                                                               let grTimesLeaf =   grPermanent*-leafPermanent
+                                                                if(1==0) for(n=2;n<6            ;n++)//still runs at higher n (2000ish)
                                                                  {
-                                                                     let plusOrMinusPowerSeries = -(tAScaled**n)*Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
+                                                                     let plusOrMinusPowerSeries = -(grTimesLeaf*tAScaled**n)*Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     
                                                                      if(plusOrMinusPowerSeries!=0.)tolerance+=plusOrMinusPowerSeries;
                                                                      else{//console.log(n);
