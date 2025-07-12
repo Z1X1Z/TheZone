e1a77beae0a4df766b00ac2bda0b03e588889398
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-12 16:12:35 -0400

    the other fraction

--- starshipMod.js
+++ starshipMod.js
@@ -3686,13 +3686,13 @@ let tolerance=0;//(1024-26)/10000
 
                                                                  let plusOrMinusPowerSeries=tAScaled;
                                                                  let loopsThresh = 1
-                                                                 if(1==1) for(n=1;n<50;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
+                                                                 if(1==1) for(n=1;n<51;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
                                                                  {
                                                                      let sig = Math.sign(n%2-.5);
                                                   console.log(n)
                                                                 
                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh
-                                                                                        -(tAScaled/plusOrMinusPowerSeries+plusOrMinusPowerSeries*tAScaled)*sig))
+                                                                                        -(plusOrMinusPowerSeries/tAScaled+tAScaled*plusOrMinusPowerSeries)*sig))
                                                                  //     plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh-3.*(plusOrMinusPowerSeries-plusOrMinusPowerSeries*totalAMP)*sig))//*grTimesLeaf
                                                                                                                ))*sig;//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     loopsThresh++
