b533c0686284725d54cee2d9fd96c786be0d71c7
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-11 22:06:05 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3681,7 +3681,7 @@ let tolerance=0;//(1024-26)/10000
                                                                  // console.log(tAScaled)
                                                         //       let grTimesLeaf =   grPermanent*-leafPermanent
                                                       // let feedbackVal = grTimesLeaf;
-                                                                 if(1==1) for(n=3;n<67            ;n++)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
+                                                                 if(1==1) for(n=3;n<75            ;n++)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
                                                                  {
                                                   
                                                                      let plusOrMinusPowerSeries = (tAScaled**(2**(-1./n)//*grTimesLeaf
