5a9dfa6c234c975a23194c6d6ba79848b5c24adb
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-12 16:53:30 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3686,13 +3686,13 @@ let tolerance=0;//(1024-26)/10000
 
                                                                  let plusOrMinusPowerSeries=tAScaled;
                                                                  let loopsThresh = 1
-                                                                 if(1==1) for(n=1;n<52;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
+                                                                 if(1==1) for(n=0;n<51;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
                                                                  {
                                                                      let sig = Math.sign(n%2-.5);
                                                   console.log(n)
                                                                 
-                                                                     plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh
-                                                                                        -1./(plusOrMinusPowerSeries+tAScaled*plusOrMinusPowerSeries)*sig))
+                                                                     plusOrMinusPowerSeries = -(tAScaled**(2**(-1.5/(loopsThresh
+                                                                                        -(plusOrMinusPowerSeries+tAScaled*plusOrMinusPowerSeries)*sig))
                                                                  //     plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh-3.*(plusOrMinusPowerSeries-plusOrMinusPowerSeries*totalAMP)*sig))//*grTimesLeaf
                                                                                                                ))*sig;//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     loopsThresh++
