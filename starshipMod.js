57f66524f01e61414d81b598a4f1615446099f00
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-12 13:02:09 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3684,12 +3684,12 @@ let tolerance=0;//(1024-26)/10000
                                                       // let feedbackVal = grTimesLeaf;
                                                                 // console.log(tAScaled)
 
-                                                                 let plusOrMinusPowerSeries=tAScaled;
+                                                                 let plusOrMinusPowerSeries=0.;tAScaled;
                                                                  let loopsThresh = 1
-                                                                 if(1==1) for(n=1;n<11;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
+                                                                 if(1==1) for(n=1;n<2;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
                                                                  {
-                                                  
-                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1./(loopsThresh+plusOrMinusPowerSeries-tAScaled*plusOrMinusPowerSeries))//*grTimesLeaf
+                                                  console.log(n)
+                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1./(loopsThresh-tAScaled))//*grTimesLeaf
                                                                                                                ))*Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     loopsThresh++
                                                                      if(plusOrMinusPowerSeries!=1.)tolerance+=plusOrMinusPowerSeries;
