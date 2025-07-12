3fd507bf990a336693c9403bdaeb164bf9d3f0ca
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-12 18:03:17 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3692,7 +3692,7 @@ let tolerance=0;//(1024-26)/10000
                                                   console.log(plusOrMinusPowerSeries)
                                                                 
                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh
-                                                                                        +(plusOrMinusPowerSeries+tAScaled*plusOrMinusPowerSeries)*sig))
+                                                                                                        -(plusOrMinusPowerSeries+tAScaled*plusOrMinusPowerSeries)*sig*1.5))
                                                                  //     plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh-3.*(plusOrMinusPowerSeries-plusOrMinusPowerSeries*totalAMP)*sig))//*grTimesLeaf
                                                                                                                ))*sig;//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     loopsThresh++
