28162b484c99c8f669d855465b42db47a466f658
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-12 13:25:08 -0400

    back here!

--- starshipMod.js
+++ starshipMod.js
@@ -3686,12 +3686,12 @@ let tolerance=0;//(1024-26)/10000
 
                                                                  let plusOrMinusPowerSeries=tAScaled;
                                                                  let loopsThresh = 1
-                                                                 if(1==1) for(n=1;n<21;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
+                                                                 if(1==1) for(n=1;n<65;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
                                                                  {
                                                                      let sig = Math.sign(n%2-.5);
                                                   console.log(n)
-                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh+3*tAScaled))//*grTimesLeaf
-                                                                                                               ))*Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
+                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh-3*tAScaled*sig))//*grTimesLeaf
+                                                                                                               ))*sig;//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     loopsThresh++
                                                                      if(plusOrMinusPowerSeries!=1.)tolerance+=plusOrMinusPowerSeries;
                                                                      else{console.log(n);
