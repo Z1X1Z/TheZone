4e5854641be3c61b9efdb3970bcd9b0cbc9fb879
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-12 12:00:10 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3689,7 +3689,7 @@ let tolerance=0;//(1024-26)/10000
                                                                  if(1==1) for(n=1;n<65;n++)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
                                                                  {
                                                   
-                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1./(n+(plusOrMinusPowerSeries*tAScaled)))//*grTimesLeaf
+                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1./(n+plusOrMinusPowerSeries-(plusOrMinusPowerSeries*tAScaled)))//*grTimesLeaf
                                                                                                                ))*Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
                                                         //             loopsThresh++
                                                                      if(plusOrMinusPowerSeries!=1.)tolerance+=plusOrMinusPowerSeries;
