8cf7f606c940e01ca8905fdddb6588345db5556b
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-12 17:51:41 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3683,16 +3683,16 @@ let tolerance=0;//(1024-26)/10000
                                                         //       let grTimesLeaf =   grPermanent*-leafPermanent
                                                       // let feedbackVal = grTimesLeaf;
                                                                 // console.log(tAScaled)
-                                                //                 tAScaled=tAScaled**.5;
+                                                 //                tAScaled=tAScaled**.5;
                                                                  let plusOrMinusPowerSeries=tAScaled;
                                                                  let loopsThresh = 1
-                                                                 if(1==1) for(n=1;n<11;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
+                                                                 if(1==1) for(n=1;n<17;n+=1)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
                                                                  {
                                                                      let sig = Math.sign(n%2-.5);
-                                                  console.log(n)
+                                                  console.log(plusOrMinusPowerSeries)
                                                                 
-                                                                     plusOrMinusPowerSeries = (tAScaled**(1+2**(-1.5/(loopsThresh
-                                                                                        -(plusOrMinusPowerSeries+tAScaled*plusOrMinusPowerSeries)*sig))
+                                                                     plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh
+                                                                                        +(plusOrMinusPowerSeries+tAScaled*plusOrMinusPowerSeries)*sig))
                                                                  //     plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(loopsThresh-3.*(plusOrMinusPowerSeries-plusOrMinusPowerSeries*totalAMP)*sig))//*grTimesLeaf
                                                                                                                ))*sig;//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     loopsThresh++
