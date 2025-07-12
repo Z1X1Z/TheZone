b9d7e082bfffc091265986c7a43c270a3a859252
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-12 09:35:25 -0400

    square was effective,
    this seems to make this fairly rational!

--- starshipMod.js
+++ starshipMod.js
@@ -3685,7 +3685,7 @@ let tolerance=0;//(1024-26)/10000
                                                                  if(1==1) for(n=1;n<101;n++)//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
                                                                  {
                                                   
-                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(n+plusOrMinusPowerSeries))//*grTimesLeaf
+                                                                      plusOrMinusPowerSeries = (tAScaled**(2**(-1.5/(n+(plusOrMinusPowerSeries*tAScaled)))//*grTimesLeaf
                                                                                                                ))*Math.sign(n%2-.5);//x-x**2+x**3-x**4....//may have an algebraic solution
                                                                     
                                                                      if(plusOrMinusPowerSeries!=1.)tolerance+=plusOrMinusPowerSeries;
