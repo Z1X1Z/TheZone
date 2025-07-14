75bdd897f645390ed03833406509aa49f4063921
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-14 03:02:14 -0400

    2nd phase of pitch detection dynamized

--- starshipMod.js
+++ starshipMod.js
@@ -3673,7 +3673,10 @@ let tolerance=0;//(1024-26)/10000
 
                                                                  
                                                                  if(tAScaled>0&&isFinite(tAScaled))
-                                                                     for(var reps=0; reps<2.;reps+=1)
+                                                                   //  for(var reps=0; reps<2.;reps+=1)
+                                                                         if(totalAMP<.5-.01)
+                                                                              for(reps=0;reps<3;reps+=.75+1./(1.-totalAMP))
+
                                                                      { tAScaled=((tAScaled**(1.-tAScaled)+tAScaled**(1.+tAScaledPermanent))/(2.-tAScaledPermanent))
                                                                          let taEX1 = ((1.-tAScaled)*(1.+tAScaledPermanent));
                                                                          tAScaled=tAScaled**(Math.sign(taEX1)*(Math.abs(taEX1))**(
@@ -3694,7 +3697,13 @@ let tolerance=0;//(1024-26)/10000
                                                                  let plusOrMinusPowerSeries=tAScaled;
                                                                  let plusOrMinusPowerSeriesBUFFER=plusOrMinusPowerSeries;
                                                                  let loopsThresh = 1
-                                                                 if(totalAMP<.5-.1) for(n=0;n<444;n+=2.-1./(1.-totalAMP))//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
+                                                                if(totalAMP<.5-.01)
+                                                                     for(n=0;n<444;n+=2.-1./(1.-totalAMP))
+
+                                                                 
+                                                                   //  for(n=0;n<444;n+=2.-totalAMP//1./(1.+1./totalAMP)
+                                                                       //  )//still runs as n gets very large, maybe 2 to odd?even?//14 works well, 4 doesn;t
+                                                                 
                                                                  {
                                                                      let sig = Math.sign(loopsThresh%2-.5);
                                                   //console.log(plusOrMinusPowerSeries)
