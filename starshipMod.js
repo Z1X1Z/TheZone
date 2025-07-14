05ee187db06b3ab5d872a0446024fced28747af3
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-14 03:13:00 -0400

    more responsive

--- starshipMod.js
+++ starshipMod.js
@@ -3698,7 +3698,7 @@ let tolerance=0;//(1024-26)/10000
                                                                  let plusOrMinusPowerSeriesBUFFER=plusOrMinusPowerSeries;
                                                                  let loopsThresh = 1
                                                                 if(totalAMP<.5-.01)
-                                                                     for(n=0;n<444;n+=2.-1./(1.-totalAMP))
+                                                                     for(n=0;n<888;n+=2.-1./(1.-totalAMP))
 
                                                                  
                                                                    //  for(n=0;n<444;n+=2.-totalAMP//1./(1.+1./totalAMP)
