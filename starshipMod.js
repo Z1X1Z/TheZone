bcc5a7958d81515a91ad19da971ddcaed9877424
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-15 20:26:09 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3738,7 +3738,7 @@ let tolerance=0;//(1024-26)/10000
                                                                  }
                                                           
                                                         //       if(  loopsThresh%2==0 )
-                                                                                                                     tolerance=tolerance**((totalAMP+tAScaled+tolerance)*2);
+                                                                                                                     tolerance=tolerance**((totalAMP+tAScaled+tolerance)*1.5);
 
                                                                  /*
                                                                 else{
