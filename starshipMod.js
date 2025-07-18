5fe472bb1e43c546521cb260f20848435062910c
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-17 20:51:22 -0400

    .5 was hiccupping at very low on ipad, now .75

--- starshipMod.js
+++ starshipMod.js
@@ -3556,7 +3556,6 @@ for(var n = 0; n<targets.length;n++){
                                                              if(!window.touchOnlyMode)audioX.suspend();
                                                              if(typeof Wad=="function") Wad.audioContext.suspend();
                                                              if(typeof wakeLock == "object")wakeLock.release();
-                                                             
                                                          }
                                                          else {
                                                              //requestWakeLock();
@@ -3740,7 +3739,7 @@ let tolerance=0;//(1024-26)/10000
                                                                  }
                                                           
                                                         //       if(  loopsThresh%2==0 )
-                                                                                                                     tolerance=tolerance**((.5+totalAMP+tAScaled+tolerance));
+                                                                                                                     tolerance=tolerance**((.75+totalAMP+tAScaled+tolerance));
 
                                                                  /*
                                                                 else{
