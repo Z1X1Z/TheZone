dc285db088c351ab8d70bad734fe9811008077c4
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-18 10:35:29 -0400

    **tolerance**2 wasn't working on ipad low ranges

--- starshipMod.js
+++ starshipMod.js
@@ -3738,7 +3738,7 @@ let tolerance=0;//(1024-26)/10000
                                                                  }
                                                           
                                                         //       if(  loopsThresh%2==0 )
-                                                                                                                     tolerance=tolerance**(((.5+totalAMP+tAScaledPermanent+tolerance**2)));
+                                                                                                                     tolerance=tolerance**(((.5+totalAMP+tAScaledPermanent+tolerance)));
 
                                                                  /*
                                                                 else{
