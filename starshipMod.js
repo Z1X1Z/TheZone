ed16df02662fbecb40f670927093f25d4491740e
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-17 20:58:56 -0400

    handles better at .75

--- starshipMod.js
+++ starshipMod.js
@@ -3739,7 +3739,7 @@ let tolerance=0;//(1024-26)/10000
                                                                  }
                                                           
                                                         //       if(  loopsThresh%2==0 )
-                                                                                                                     tolerance=tolerance**((2./3.+totalAMP+tAScaled+tolerance));
+                                                                                                                     tolerance=tolerance**((.75+totalAMP+tAScaled+tolerance));
 
                                                                  /*
                                                                 else{
