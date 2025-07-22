88dade169e693160f9f709b2296810a53d9f9584
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-07-22 16:10:59 -0400

    <445

--- starshipMod.js
+++ starshipMod.js
@@ -3753,8 +3753,7 @@ let tolerance=0;//(1024-26)/10000
                                                                      loopsThresh++
 
                                                                  }
-                                                                 tolerance=(tolerance-plusOrMinusPowerSeriesNorm)**(.75+(totalAMP+ tAScaledPermanent+tolerance+plusOrMinusPowerSeriesBUFFER));
-                                                      //  tolerance=(tolerance+plusOrMinusPowerSeriesBUFFER)**(.75+(totalAMP+ tAScaledPermanent+tolerance-plusOrMinusPowerSeries));
+                                                        tolerance=(tolerance+plusOrMinusPowerSeriesBUFFER)**(.75+(totalAMP+ tAScaledPermanent+tolerance-plusOrMinusPowerSeriesNorm));
                                                            //      tolerance=(tolerance-plusOrMinusPowerSeries)**(.5+(totalAMP+ tAScaledPermanent+tolerance+plusOrMinusPowerSeriesBUFFER));
                                                           
                                                              
