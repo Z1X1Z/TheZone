6fc9bc527e106371c58455fc6662ef512f317e28
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-11-21 16:23:03 -0500

    Add files via upload

--- hotkeys.js
+++ hotkeys.js
@@ -216,7 +216,7 @@ noteFrozen:{value:0},
       ringSpring:{value:true},
       squeezeN:{value:1.},
 seventhEYE:{value:true},
-polyNomialStretch:{value:false   }
+polyNomialStretch:{value:true   }
 
 }
 window.uniforms={}
