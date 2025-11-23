0041e7d8ad8b8302f36a59a4df162edeec5fb355
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-11-23 16:05:09 -0500

    stable rift

--- bigTreeOfLife.js
+++ bigTreeOfLife.js
@@ -479,12 +479,13 @@ s.x*s.x*s.x  - 3.*s.x*s.y*s.y,
 -s.y*s.y*s.y+ 3.*s.x*s.x*s.y
 );
     
-if(zoom<=.5&&counter==0.&&polyNomialStretch){
+if(//zoom<=.5&&counter==0.&&
+    polyNomialStretch){
     /* if(seventhEYE==0.||lfc>2./3.)
             s.multiplyScalar((lfc+1.)/2.);//maybe other values work?
     else s.multiplyScalar(lfc/2.+1./4.);//maybe other values work?
     */
-                s.multiplyScalar(lfc+.5);//maybe other values work?
+                s.multiplyScalar(lfc+1.);//maybe other values work?
 
 }
 
