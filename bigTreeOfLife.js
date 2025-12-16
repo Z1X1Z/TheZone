2ded666d889cc083825fc0e869b99fd5e2db58c3
Z1X1Z <hallelujah.hephzibah@gmail.com>
2025-12-15 22:28:01 -0500

    Add files via upload

--- bigTreeOfLife.js
+++ bigTreeOfLife.js
@@ -360,7 +360,7 @@ else{
    }
    var spokeloverON=spokelover&&zoom/lfc<1.;
 
-     if(spokeloverON)hyperCore-=Math.log(2);
+     if(spokeloverON)hyperCore+=Math.log(2);
 
      if(squeezeN>1.)hyperCore-=1.25;
 //hyperCore-=1.;
