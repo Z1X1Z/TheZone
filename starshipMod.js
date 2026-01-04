c02caa0292efdb41395f4a5198d9ca3d7c82c627
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-04 15:24:57 -0500

    last not working well on mobile, trying *2

--- starshipMod.js
+++ starshipMod.js
@@ -4048,7 +4048,6 @@ let tolerance=0;//(1024-26)/10000
                                                             tolerance=(toleranceFixed)**((1+tolerance))
                                                            // ll++
                                                            }
-
                                                 tolerance=(tolerance/trunc)*trunc
                                                             toleranceFixed = tolerance;
                                                     for(var vvv = 0.; vvv<1.;vvv+=tAScaledPermanent)
@@ -4056,10 +4055,9 @@ let tolerance=0;//(1024-26)/10000
                                                             tolerance=(toleranceFixed)**((1+tolerance))
                                                            // ll++
                                                            }
-                                                        tolerance*=(-leafPermanent/trunc)*trunc;//makes over and under stable and greatly enhances accuracy
+                                            tolerance*=(((-leafPermanent*2.)/trunc)*trunc);//makes over and under stable and greatly enhances accuracy
 
                                                 tolerance=(tolerance/trunc)*trunc
-
 
                                                          //   console.log(ll)
 
