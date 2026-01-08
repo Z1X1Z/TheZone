350ec2e9a82eb7fcec40c81d1ad2782e997100a2
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-07 20:26:52 -0500

    polygons clumping

--- starshipMod.js
+++ starshipMod.js
@@ -3455,7 +3455,7 @@ for(let n = 0; n < polygons.length; n++)
                                                          }
                                                          if(isFinite(interpolation)&&interpolation>0.)
                                                          {
-                                                         var slowDown = .99**(1./interpolation);
+                                                         var slowDown = .98**(1./interpolation);
                                                          polygons[n].dx*=slowDown;
                                                          polygons[n].dy*=slowDown;
                                                          }
