b27212f736d56df768a6bbf3a47d8ce1594ef80d
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-09 22:46:56 -0500

    snaps on phone

--- starshipMod.js
+++ starshipMod.js
@@ -4093,7 +4093,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
                                                             toleranceFixed = tolerance;
                                                     for(var vvv = 0.; vvv<1.5;vvv+=tAScaledPermanent)
                                                            {
-                                                            tolerance=(tolerance)**((1-toleranceFixed))
+                                                            tolerance=(toleranceFixed)**((1-tolerance))
                                                             
                                                            // ll++
                                                            }
