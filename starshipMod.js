5e65842f886cf86e9401c029cf23208bf1153766
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-26 16:46:36 -0500

    iPad is hiccuping on low C

--- starshipMod.js
+++ starshipMod.js
@@ -4302,7 +4302,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
 
                                               tolerance=((tolerance)/trunc)*trunc
 
-                                                       tolerance=(tolerance)**(((((-leafPermanent+1)*(grPermanent+1))/trunc)*trunc)*((totalAMPmodified**.5+totalAMP**.5)/2.)**.5+(((totalAMPmodified**2+totalAMP**2)/2.))**2.) // this greatly improves trueness
+                                                       tolerance=(tolerance)**(((((-leafPermanent+1)*(grPermanent+1))/trunc)*trunc)*((totalAMPmodified**.5+totalAMP**.5)/2.)**2.+(((totalAMPmodified**2+totalAMP**2)/2.))**.5) // this greatly improves trueness
                                                        
                                                        //    console.log(tolerance)
                                                          //                                                  tolerance=((tolerance)/trunc)*trunc
