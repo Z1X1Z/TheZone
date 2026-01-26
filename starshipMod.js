3bd36ebb9d40c3f45877678a8efee79a59ba8e9a
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-26 18:38:12 -0500

    hopefully works on phone and iPad, hears through..
    ..noise better

--- starshipMod.js
+++ starshipMod.js
@@ -4299,8 +4299,8 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
 
                                               tolerance=((tolerance)/trunc)*trunc
                                                        tolerance=(tolerance)**((((((-leafPermanent+1)*(grPermanent+1))/trunc)*trunc)*((totalAMPmodified**.5+totalAMP**.5)/2.)**2.+(((totalAMPmodified**2+totalAMP**2)/2.))**.5)) // this greatly improves trueness
-                              let phrase = (tolerance)**2+tolerance
-                                                       tolerance=(tolerance)**((Math.E)**(phrase**phrase))
+                              let phrase = (tolerance+totalAMPmodified)**2*2.
+                                                       tolerance=(tolerance)**((Math.E)**((phrase**phrase)))
                                   
 
                                                        //    console.log(tolerance)
