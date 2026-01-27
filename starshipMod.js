1516cc5501cbe9ebede28ade73783e2f55c9f781
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-27 01:03:55 -0500

    truncating around /2

--- starshipMod.js
+++ starshipMod.js
@@ -4337,7 +4337,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
 
                                                                phrase=((phrase)/trunc)*trunc
                              phrase=(phrase)/2
-                                                                                    //            phrase=((phrase)/trunc)*trunc
+                                                                                              phrase=((phrase)/trunc)*trunc
                               //  phrase/=2
                                                            //    tolerance=((tolerance)/trunc)*trunc
 
