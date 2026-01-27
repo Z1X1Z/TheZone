919aa8562a9c546728379e5f6b65a6ca43dce7e1
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-26 20:01:49 -0500

    powerseries

--- starshipMod.js
+++ starshipMod.js
@@ -4299,7 +4299,18 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
 
                                               tolerance=((tolerance)/trunc)*trunc
                                                        tolerance=(tolerance)**((((((-leafPermanent+1)*(grPermanent+1))/trunc)*trunc)*((totalAMPmodified**.5+totalAMP**.5)/2.)**2.+(((totalAMPmodified**2+totalAMP**2)/2.))**.5)) // this greatly improves trueness
-                              let phrase = ((tolerance+totalAMPmodified))**2+((tolerance+totalAMPmodified))**4;
+                              let phrase = 0;//((tolerance+totalAMPmodified))**2-((tolerance+totalAMPmodified))**4+((tolerance+totalAMPmodified))**8;
+                              let plusOrMinus = 1;
+                              let term = 1;
+                              let sum =tolerance+totalAMPmodified
+                              for(var b = 1;b<10000;b++)
+                                {
+                                     term = (sum)**(b)*plusOrMinus;
+                                    phrase+=term
+                                    plusOrMinus*=-1
+                                    if(term==0)break
+                                    loops++
+                                }
                                                                                      tolerance=(tolerance)**((Math.E)**((phrase**phrase)))
                                   
 
