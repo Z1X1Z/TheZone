7ee8492fcf3fe0196e54d16b911823bf8a17ae81
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-27 02:15:18 -0500

    sum = tolerance+totalAMPmodified+tAScaledPermanent

--- starshipMod.js
+++ starshipMod.js
@@ -4305,7 +4305,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
 
                                                        let plusOrMinus = 1;
                               let term = 1;
-                              let sum =tolerance+totalAMPmodified//+tAScaledPermanent
+                              let sum =tolerance+totalAMPmodified+tAScaledPermanent
 
 /*
                               let phrase = 0;//((tolerance+totalAMPmodified))**2-((tolerance+totalAMPmodified))**4+((tolerance+totalAMPmodified))**8;
@@ -4325,7 +4325,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
                                                               let phrase =0;//((tolerance+totalAMPmodified+tAScaledPermanent))**2+((tolerance+totalAMPmodified+tAScaledPermanent))**4;
        for(var b = 1;b<10000;b+=2)
                                 {
-                                     term = (sum)**(2**b)*plusOrMinus;
+                                     term = (sum)**(b)*plusOrMinus;
                                //  term=((term)/trunc)*trunc
                                     phrase+=term
                                                    //                phrase=((phrase)/trunc)*trunc
