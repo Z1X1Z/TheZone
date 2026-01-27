23fbf4e660e0142b1f2893bb3ad1f04c38c3a759
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-27 15:54:06 -0500

    trying with one more truncator(phrase)

--- starshipMod.js
+++ starshipMod.js
@@ -4300,7 +4300,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
                                               tolerance=((tolerance)/trunc)*trunc
                                                        tolerance=(tolerance)**((((((-leafPermanent+1)*(grPermanent+1))/trunc)*trunc)*((totalAMPmodified**.5+totalAMP**.5)/2.)**2.+(((totalAMPmodified**2+totalAMP**2)/2.))**.5)) // this greatly improves trueness
 
-                      //         tolerance=((tolerance)/trunc)*trunc
+                               tolerance=((tolerance)/trunc)*trunc
 
 
                                                        let plusOrMinus = 1;
@@ -4338,7 +4338,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
                             }
                          //   console.log(loops)
 
-                           // phrase=((phrase)/trunc)*trunc
+                           phrase=((phrase)/trunc)*trunc
                              //  phrase/=2
                                                         //   tolerance=((tolerance)/trunc)*trunc
                             phrase= phrase**phrase
