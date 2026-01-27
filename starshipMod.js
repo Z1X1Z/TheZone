52f95db75f0f5700d28bbad01d0519e4211664a5
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-27 03:43:35 -0500

    it liked something like this before

--- starshipMod.js
+++ starshipMod.js
@@ -4305,7 +4305,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
 
                                                        let plusOrMinus = 1;
                               let term = 1;
-                              let sum =((totalAMPmodified+tolerance))//(tolerance+totalAMPmodified+tAScaledPermanent)
+                              let sum =((totalAMPmodified+tolerance+tAScaledPermanent))//(tolerance+totalAMPmodified+tAScaledPermanent)
 
 /*
                               let phrase = 0;//((tolerance+totalAMPmodified))**2-((tolerance+totalAMPmodified))**4+((tolerance+totalAMPmodified))**8;
