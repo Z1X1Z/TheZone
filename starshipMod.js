b8de2b11b64daa6a454a276e3a3c138c6cb81905
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-27 02:43:36 -0500

    this update happy offline

--- starshipMod.js
+++ starshipMod.js
@@ -4330,7 +4330,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
                                     phrase+=term
                                                    //                phrase=((phrase)/trunc)*trunc
 
-                                    plusOrMinus*=-1
+                                  //  plusOrMinus*=-1
                                     if(term==0)
                                         { loops = b
                                             break
