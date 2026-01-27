a565a6da490ac5c00fce2e638e4fb0b69419aabb
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-26 20:09:52 -0500

    tolerance+totalAMPmodified+tAScaledPermanent

--- starshipMod.js
+++ starshipMod.js
@@ -4299,10 +4299,11 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
 
                                               tolerance=((tolerance)/trunc)*trunc
                                                        tolerance=(tolerance)**((((((-leafPermanent+1)*(grPermanent+1))/trunc)*trunc)*((totalAMPmodified**.5+totalAMP**.5)/2.)**2.+(((totalAMPmodified**2+totalAMP**2)/2.))**.5)) // this greatly improves trueness
-                              let phrase = 0;//((tolerance+totalAMPmodified))**2-((tolerance+totalAMPmodified))**4+((tolerance+totalAMPmodified))**8;
-                              let plusOrMinus = 1;
+                                                           let phrase = 0;//((tolerance+totalAMPmodified))**2-((tolerance+totalAMPmodified))**4+((tolerance+totalAMPmodified))**8;
+
+                                                       let plusOrMinus = 1;
                               let term = 1;
-                              let sum =tolerance+totalAMPmodified
+                              let sum =tolerance+totalAMPmodified+tAScaledPermanent
                               for(var b = 1;b<10000;b++)
                                 {
                                      term = (sum)**(b)*plusOrMinus;
