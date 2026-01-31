4154b0843848e8ba5e6b40ba185423b06cd38e1b
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-01-31 02:59:50 -0500

    totalAMP**(totalAMP/4.)*totalAMP+tolerance**powerAMP+totalAMP
    really smooth and noise resistant offline

--- starshipMod.js
+++ starshipMod.js
@@ -4378,7 +4378,7 @@ totalAMPmodified = (((totalAMPmodified)/preTrunc)*preTrunc)
                            // let nudgeAdjust = totalAMP;
                             
                            // for (var na = 0; na<3; na+=1)nudgeAdjust=totalAMP**nudgeAdjust
-                                    tolerance+=(totalAMP)**(totalAMP/4.)*totalAMP+tolerance**powerAMP//*totalAMP**(powerAMP/3).;///4./fractionOfFrame may actually be a fixed 256 bytes
+                                    tolerance+=(totalAMP)**(totalAMP/4.)*totalAMP+tolerance**(1.+tolerance)//*totalAMP**(powerAMP/3).;///4./fractionOfFrame may actually be a fixed 256 bytes
                                                                 //             tolerance=((tolerance)/trunc)*trunc
 
                                                    //                 tolerance=((tolerance)/trunc)*trunc
