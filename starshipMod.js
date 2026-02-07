b6ab8b50c938fa004305b4578baae01eb963e4bf
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-02-07 00:20:59 -0500

    it seems to be partly (largely?) illusory

--- starshipMod.js
+++ starshipMod.js
@@ -4467,9 +4467,11 @@ totalAMPmodified = (((totalAMPmodified)/trunc)*trunc)
                            // let nudgeAdjust = totalAMP;
                             
                            // for (var na = 0; na<3; na+=1)nudgeAdjust=totalAMP**nudgeAdjust
-                               if (   !window.iOS )   
-                                  tolerance+=(totalAMP)**(totalAMP/4)*totalAMP+(totalAMP)**((totalAMP)*((2**.5)*3*4))+((-leafPermanent+grPermanent+2**.5)-4./3.*8.*(totalAMP/2.+totalAMPmodified))/fractionOfFrame/2.//-totalAMPmodified/fractionOfFrame/2//tolerance+=(totalAMP)**(totalAMP/4)*totalAMP+totalAMP**((totalAMP)*(6*2*(2**.5)))
-                                     
+                               if (   !window.iOS )
+                               {
+                                  tolerance+=(totalAMP)**(totalAMP/4)*totalAMP+(totalAMP)**((totalAMP)*((2**.5)*3*4))//-totalAMPmodified/fractionOfFrame/2//tolerance+=(totalAMP)**(totalAMP/4)*totalAMP+totalAMP**((totalAMP)*(6*2*(2**.5)))
+                                    tolerance +=((-leafPermanent+grPermanent+2**.5)*1.5-2.*(totalAMP/2.+totalAMPmodified))/fractionOfFrame//this line is uncertain, particularly coefficients
+                               }
                                     //console.log(7.5*Number.MIN_VALUE)
                                     //alsotolerance**(powerAMP**2*6) ; //*totalAMP**(powerAMP/3).;///4./fractionOfFrame may actually be a fixed 256 bytes
                                                                 //             tolerance=((tolerance)/trunc)*trunc
