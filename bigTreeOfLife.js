8ea84d2695c438ccbd1ebc1e63f9380c113df9cd
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-02-21 21:27:26 -0500

    less drift

--- bigTreeOfLife.js
+++ bigTreeOfLife.js
@@ -217,8 +217,8 @@ if(seventhEYEandJubileye)
 
          if(lengthP<1./3.)
      {
-    swapped=(swap-swapJUBILEE+swapDEFAULT+2)%4.;
-   flopped =(oppositionalCoreFlop- oppositionalCoreFlopJUBILEE+ oppositionalCoreFlopDEFAULT+1)%3.;
+    swapped=(swap-swapJUBILEE+swapDEFAULT+3)%4.;
+   flopped =(oppositionalCoreFlop- oppositionalCoreFlopJUBILEE+ oppositionalCoreFlopDEFAULT+0)%3.;
 }
         else if (lengthP>2./3.)
         {
