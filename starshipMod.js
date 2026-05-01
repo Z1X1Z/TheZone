50a6c29737fb4da61d93657332bbb27de1df7491
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-05-01 18:04:28 -0400

    last update still not hearing well on phone

--- starshipMod.js
+++ starshipMod.js
@@ -4545,7 +4545,7 @@ totalAMPmodified = (((totalAMPmodified)/trunc)*trunc)
 
             tolerance=tolerance**((2**.5/trunc)*trunc)**tolerance;
 
-           // tolerance=(tolerance/trunc)*trunc
+         tolerance=(tolerance/trunc)*trunc
 
                     tolerance*=((((grPermanent)/trunc)*trunc)/(((-leafPermanent/trunc)*trunc)*(2**.5))/trunc)*trunc;
             tolerance=(tolerance/trunc)*trunc
