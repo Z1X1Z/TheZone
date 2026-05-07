5f893a452686e3d4ac5d0abc89ed30bd6d53794f
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-05-07 06:15:38 -0400

    hears low ranges smoother

--- starshipMod.js
+++ starshipMod.js
@@ -4550,12 +4550,9 @@ totalAMPmodified = (((totalAMPmodified)/trunc)*trunc)
             tolerance=(tolerance/trunc)*trunc
                     tolerance*=((((grPermanent)/trunc)*trunc)/(((-leafPermanent/trunc)*trunc)*(2**.5/trunc)*trunc)/trunc)*trunc;
                     for(var m=0;m<1.;m+=totalAMP) 
-                    {tolerance=tolerance**tolerance;
+                    {tolerance=tolerance**(.75+tolerance);
                     }
-                           tolerance=(tolerance/trunc)*trunc
-
-
-
+                          tolerance=(tolerance/trunc)*trunc
 }
 
                                
