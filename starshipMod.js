03043c0b616948f6f26827b2f3d8ac517e62ee9e
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-06-04 23:51:02 -0400

    big improvement to pitch detection

--- starshipMod.js
+++ starshipMod.js
@@ -4566,7 +4566,7 @@ totalAMPmodified = (((totalAMPmodified)/trunc)*trunc)
                           tolerance=(tolerance/trunc)*trunc
                            if(window.highORlow!=3.)
 {
-                          tolerance*=totalAMP**totalAMP;
+                          tolerance*=(totalAMP**totalAMP)**(1.+totalAMP-tolerance);//not exhaustively optimized, but intuitive and effective
 
 }
                                
