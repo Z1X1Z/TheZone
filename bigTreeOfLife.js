c40eeed0be7ca26bde060f0ef80d5e2df47584d1
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-01 23:30:38 -0400

    moore cores=more clovers

--- bigTreeOfLife.js
+++ bigTreeOfLife.js
@@ -542,7 +542,7 @@ hyperCore+=.5;
 if(jubileyes!=0.) 
 {
 if((seventhEYEandJubileye&&lengthP<1./3.)||(!seventhEYEandJubileye&&seventhEYE==3.&&lengthP<2./3.   ))
-hyperCore+=4./3.;
+hyperCore+=1.;
 //else if (seventhEYE!=3.||lengthP<2./3.)
 else if((seventhEYE==0.||(seventhEYEandJubileye&&lengthP<2./3.))&&(lengthP<SEVEYEStartAdj||!seventhOUTside))
 hyperCore+=.5;
@@ -551,7 +551,7 @@ hyperCore+=.5;
 if(clvrVariant4!=0.&&(lengthP<SEVEYEStartAdj||!seventhOUTside)) 
  {
     hyperCore-=lfcCenterCored*variant4Correction-1.;
-    hyperCore+=.75;
+    hyperCore+=1.;
  }
 
 
