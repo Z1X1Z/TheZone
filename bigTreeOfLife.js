8f51b291c547733e65c9986cb23899ed79949dc0
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-05 23:49:48 -0400

    Add files via upload

--- bigTreeOfLife.js
+++ bigTreeOfLife.js
@@ -1221,7 +1221,7 @@ jubileyes!=0.&&
 
         if(seventhEYEthree||(seventhEYE==3.&&lengthP<1./3.))s=new THREE.Vector2(s.x,abs(s.y));
         else s=new THREE.Vector2(s.x,abs(s.y)-.5);
-    s=new THREE.Vector2(1.5/(2.5+s.x),1.5/(2.5-Math.abs(s.y)+budge-.5));
+    s=new THREE.Vector2(1.5/(2.5-Math.abs(s.x)),1.5/(2.5-Math.abs(s.y)+budge-.5));
         if(jubileyes<0.)s=new THREE.Vector2(s.y,Math.abs(s.x));//more sign checks may help
 
    // try signs and swizzle!
