6b54951335b12b4535b1ddcfb5678cc09a821d3f
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-08 22:46:07 -0400

    there was an extra trunc

--- starshipMod.js
+++ starshipMod.js
@@ -4568,7 +4568,7 @@ totalAMPmodified = (((totalAMPmodified)/trunc)*trunc)
                         //last line working on nyquist filtering, added
                         nyquistFilter = false;//turn off to debug nyquists
                   tolerance+=toleranceNudge;
-            tolerance=(tolerance/trunc)*trunc
+          //  tolerance=(tolerance/trunc)*trunc
 
             tolerance=tolerance**((2**.5/trunc)*trunc)**tolerance;
 
