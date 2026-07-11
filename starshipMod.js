7dc8c3c30c524022da2c7b8cf3d96139326076ed
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-11 00:26:30 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -2530,10 +2530,12 @@ uniforms.movieTime.value=(window.TIMESTAMP-window.movieStartTime)/1000./window.m
                                     
                                     totalAMP = 0.;
 
-                                    for(var n=0; n<inputData.length-1;n++)totalAMP+=Math.abs(inputData[n]);
 
                                     for(var n=0; n<inputData.length-1;n++)totalAMP+=Math.sign(inputData[n])*Math.abs(inputData[n])**.5+Math.sign(inputData[n+1])*(inputData[n+1]**2);
-                                    totalAMP=Math.abs(totalAMP)/3.;
+                                    totalAMP=Math.abs(totalAMP)/2.;
+                                                                        for(var n=0; n<inputData.length-1;n++)totalAMP+=Math.abs(inputData[n]);
+                                    totalAMP=Math.abs(totalAMP)/2.;
+
 
                                         totalAMP/=inputData.length-1;
                                                        // if(window.android)totalAMP=totalAMP**.5/8.;//may not work as intended on all platforms, if at all
