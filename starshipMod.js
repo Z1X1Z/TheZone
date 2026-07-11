b7ac55b555d714d087f6cc9b941542d7ce35e6dd
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-10 23:07:58 -0400

    took off absolute value

--- starshipMod.js
+++ starshipMod.js
@@ -2529,8 +2529,8 @@ uniforms.movieTime.value=(window.TIMESTAMP-window.movieStartTime)/1000./window.m
                                     }
                                     
                                     totalAMP = 0.;
-                                    for(var n=0; n<inputData.length-1;n++)totalAMP+=(Math.abs((Math.abs(inputData[n])**.5)+(inputData[n+1]**2)));
-                                    totalAMP/=2.;
+                                    for(var n=0; n<inputData.length-1;n++)totalAMP+=(Math.sign(inputData[n])*Math.abs(inputData[n])**.5)-Math.sign(inputData[n+1])*(inputData[n+1]**2);
+                                    totalAMP=Math.abs(totalAMP)/2.;
                                         totalAMP/=inputData.length-1;
                                                        // if(window.android)totalAMP=totalAMP**.5/8.;//may not work as intended on all platforms, if at all
                                                      //   else if(iOS)totalAMP=totalAMP*2.;//may not work as intended on all platforms, if at all
