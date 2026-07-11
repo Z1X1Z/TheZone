21e6837b7c8beb06c9a190b8884f9663ba2e3553
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-11 00:14:01 -0400

    last wasn't smooth on phone, this one may not..
    ..be either
    well we'll try
    so goes

--- starshipMod.js
+++ starshipMod.js
@@ -2529,7 +2529,7 @@ uniforms.movieTime.value=(window.TIMESTAMP-window.movieStartTime)/1000./window.m
                                     }
                                     
                                     totalAMP = 0.;
-                                    for(var n=0; n<inputData.length-1;n++)totalAMP+=Math.abs(inputData[n])*Math.abs(inputData[n])**.5-Math.sign(inputData[n])*(inputData[n]**2);
+                                    for(var n=0; n<inputData.length-1;n++)totalAMP+=Math.abs(inputData[n])*Math.abs(inputData[n])**.5-Math.sign(inputData[n+1])*(inputData[n+1]**2);
                                     totalAMP=Math.abs(totalAMP)/2.;
                                     for(var n=0; n<inputData.length-1;n++)totalAMP+=inputData[n]
                                     totalAMP=Math.abs(totalAMP)/2.;
