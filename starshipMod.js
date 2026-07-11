28dd482926b755d1d32ca5ff76a303661c0b1875
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-10 23:25:40 -0400

    minus

--- starshipMod.js
+++ starshipMod.js
@@ -2529,9 +2529,9 @@ uniforms.movieTime.value=(window.TIMESTAMP-window.movieStartTime)/1000./window.m
                                     }
                                     
                                     totalAMP = 0.;
-                                    for(var n=0; n<inputData.length;n++)totalAMP+=(Math.sign(inputData[n])*Math.abs(inputData[n])**.5)+Math.sign(inputData[n])*(inputData[n]**2);
-                                    totalAMP=Math.abs(totalAMP);
-                                        totalAMP/=inputData.length;
+                                    for(var n=0; n<inputData.length-1;n++)totalAMP+=(Math.sign(inputData[n])*Math.abs(inputData[n])**.5)-Math.sign(inputData[n+1])*(inputData[n+1]**2);
+                                    totalAMP=Math.abs(totalAMP)/2.;
+                                        totalAMP/=inputData.length-1;
                                                        // if(window.android)totalAMP=totalAMP**.5/8.;//may not work as intended on all platforms, if at all
                                                      //   else if(iOS)totalAMP=totalAMP*2.;//may not work as intended on all platforms, if at all
                                     uniforms["totalAmp" ].value=totalAMP;
