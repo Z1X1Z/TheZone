83e8fb57e63b12e917eb76a936a1ff68ad5a3018
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-12 21:47:51 -0400

    something seems to be symmetry breaking
    maybe upwards oscillations are heard differently than downwards?

--- starshipMod.js
+++ starshipMod.js
@@ -2532,10 +2532,12 @@ uniforms.movieTime.value=(window.TIMESTAMP-window.movieStartTime)/1000./window.m
 
                                                                         
                                     for(var n=0; n<inputData.length-1;n++)
-                                        totalAMP+=Math.abs(inputData[n])
-                                        -(Math.abs(inputData[n])**.5)//*Math.sign(inputData[n])
-                                        +(Math.abs(inputData[n+1])**2)//Math.sign(inputData[n+1]);
-                                    totalAMP+=Math.abs(inputData[inputData.length-1])-(Math.abs(inputData[inputData.length-1])**.5)+(Math.abs(inputData[0])**2); 
+                                        totalAMP+=-Math.abs(inputData[n])
+                                        +(Math.abs(inputData[n])**.5)*Math.sign(inputData[n])
+                                        -(Math.abs(inputData[n+1])**2)*Math.sign(inputData[n+1]);
+                                    totalAMP+=-Math.abs(inputData[inputData.length-1])
+                                    +(Math.abs(inputData[inputData.length-1])**.5)*Math.sign(inputData[inputData.length-1])
+                                    -(Math.abs(inputData[0])**2)*Math.sign(inputData[0]); 
                                     totalAMP=Math.abs(totalAMP);
                                     totalAMP/=inputData.length;
                                     
