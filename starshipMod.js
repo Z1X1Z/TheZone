d6429441e0d0ffa249a4bca603a6c25886c7282a
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-13 02:35:06 -0400

    spirograph always on again
    amazing how big the difference is!  Goodness Glorious God!

--- starshipMod.js
+++ starshipMod.js
@@ -2638,7 +2638,7 @@ if( (!window.touchMode||(window.shouldShowStar))&&!window.touchOnlyMode) {
     if(true//||!DAW
 )setDynamicSampler2ds();
 
-   if(spirographMODE!=0&&pitch!=lastPitch&&on)makeSpirograph();
+   if(spirographMODE!=0&&pitch!=lastPitch)makeSpirograph();
 
 
     if (computeFPS&&fpsSET>1)
@@ -2740,7 +2740,7 @@ if( (!window.touchMode||(window.shouldShowStar))&&!window.touchOnlyMode) {
         //scene.add(line)
        let depthSpirograph =-1.;
 
-     if(spirographMODE!=0&&pitch!=lastPitch&&on&&(uniforms.movieTime.value==-1||uniforms.movieTime.value>5.) )  for (let r= 0.; r < bufferPortion; r +=1) {//spirray size supports upto r <buffersize*2
+     if(spirographMODE!=0&&pitch!=lastPitch&&(uniforms.movieTime.value==-1||uniforms.movieTime.value>5.) )  for (let r= 0.; r < bufferPortion; r +=1) {//spirray size supports upto r <buffersize*2
             const  txlast=tx;
             const  tylast=ty;
             tx = spirray0[r];
