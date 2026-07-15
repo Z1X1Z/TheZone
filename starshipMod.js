6986891e728eae6b5794a7157904267a1cee158b
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-07-14 22:46:16 -0400

    hears D better

--- starshipMod.js
+++ starshipMod.js
@@ -4313,7 +4313,7 @@ if(uniforms.coords.value.y<-.5)
                                                        
          var fractionOfFrame =1024 
          const yinData = new Float64Array(fractionOfFrame);
-                                             const wingsOfRighteousness=4.250874236524140897876;
+                                             const wingsOfRighteousness=4.250874236524140897687;
 //begin MIT license, code from https://github.com/adamski/pitch_detector
 /** Full YIN algorithm */
 function calculatePitch ()
