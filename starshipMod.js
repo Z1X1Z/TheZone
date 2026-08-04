d6f77807b2aeada214e2a4b42481349c2eed26b8
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-04 15:38:35 -0400

    wrap boost two

--- starshipMod.js
+++ starshipMod.js
@@ -754,7 +754,7 @@ window.twist-=window.spinnerTwist
                   const d_yS=spunD[1]*interpolation;
 
                 if(((coordX**2+coordY**2)**.5>uniforms.SEVEYEStart.value&&uniforms.seventhOUTside.value&&uniforms.colorCombo.value<=0))
-                    wrapMovementBoost=1.75;
+                    wrapMovementBoost=2.;
                 else wrapMovementBoost = 1;
 
            const bx=coordX+d_xS*MR*zoom*wrapMovementBoost;
@@ -2144,7 +2144,7 @@ function runOSMD (){
                                               spunTouch=spin(touchMovement,-uniforms.carousel.value*(uniforms[ "time" ].value*uniforms[ "rate" ].value+Math.PI)%(Math.PI*2.));
                                              
                 if(((coordX**2+coordY**2)**.5>uniforms.SEVEYEStart.value&&uniforms.seventhOUTside.value&&uniforms.colorCombo.value<=0.)&&spunTouch[0]!=0&&spunTouch[1]!=0)
-                    wrapMovementBoost=1.75;
+                    wrapMovementBoost=2.;
                     else wrapMovementBoost=1;
 
                                                               coordX+= spunTouch[0]*wrapMovementBoost;
