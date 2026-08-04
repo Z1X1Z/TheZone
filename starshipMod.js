a4c8b8f15415ab4b11a130d94b9b90fc1f38efb7
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-04 19:25:49 -0400

    is this working?

--- starshipMod.js
+++ starshipMod.js
@@ -918,9 +918,9 @@ function setFFTdependantSizes(){
      analyser.fftSize=fftSize;
       bufferSize = fftSize;
       numberOfBins = fftSize/2.;
-      frequencies= new Float64Array(numberOfBins);
-     inputData = new Float32Array(bufferSize);
-      dataArray = new Uint8Array( numberOfBins );
+      frequencies= new Float64Array(numberOfBins).fill(1);
+     inputData = new Float32Array(bufferSize).fill(1);
+      dataArray = new Uint8Array( numberOfBins ).fill(1);
 
      window.zoomOutRatchetThreshold=starSHIPVOLUMEdefaultLowVolume;//5./1024;////or 1/1024.//maybe shouldn't need to be here could be solved away
      
