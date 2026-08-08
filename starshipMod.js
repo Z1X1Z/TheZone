ef673f110e9f7be4af45038c4b86986faac10c7f
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-07 20:45:46 -0400

    .75 seems more responsive

--- starshipMod.js
+++ starshipMod.js
@@ -288,7 +288,7 @@ function spiral_compress(){
                 }
                             
 };
-                                      var exFactor=   .5;
+                                      var exFactor=   .75;
 var innerFrets =  new Float64Array((EldersLeg>0)?EldersLeg:0.);//could be refactored
 
 
