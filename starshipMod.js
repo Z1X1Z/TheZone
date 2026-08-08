b923ec9d8af522b6b98d2a1bc4e490105b3e83ba
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-07 20:49:45 -0400

    there's to much middle on phone

--- starshipMod.js
+++ starshipMod.js
@@ -283,7 +283,7 @@ function spiral_compress(){
 
                        if(window.extremeFrets&&EldersLeg>2)
                             {
-        for(var b = 0; b<EldersLeg; b++)if(testar[b]!=0.) testar[b]=(1.-1./(testar[b]*exFactor)**(.5))**(2)
+        for(var b = 0; b<EldersLeg; b++)if(testar[b]!=0.) testar[b]=(1.-1./(testar[b])**(.5)*exFactor)**(2)
         //    for(var b = 0; b<12; b++)if(testar[b]!=0.) stack12Array[b]=(1.-1./stack12Array[b]**(1./Math.E))**(Math.E)
                 }
                             
