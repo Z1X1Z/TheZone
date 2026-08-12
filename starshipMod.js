67470bd4f143ab7aa1a5ed7263256bdc69122f9d
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-12 06:53:50 -0400

    that was deep enough

--- starshipMod.js
+++ starshipMod.js
@@ -1786,8 +1786,8 @@ function zoomRoutine(){
 
     if(seventhOUTside&&(fromCenter-zoom)*(1.-zoom)>uniforms.SEVEYEStart.value)
     {
-            metaDepth=metaDepth*2**(uniforms.SEVEYEpow.value/(coordX**2+coordY**2)**.5+7-sealBoost )
-            metaDepth/=2**4
+            metaDepth=metaDepth*2**(uniforms.SEVEYEpow.value/(coordX**2+coordY**2)**.5+6-sealBoost )
+            
     }
     else if(seventhOUTside&&fromCenter*(1.-zoom)-zoom**.5>(uniforms.SEVEYEStart.value-1./2**uniforms.SEVEYEpow.value*uniforms.SEVEYEpow.value/2.))
         metaDepth=metaDepth*2**(uniforms.SEVEYEpow.value+6.-sealBoost)
