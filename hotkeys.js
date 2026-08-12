38f3a0485bc1cff95cef27a979ec6a8a01a3fb6e
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-11 22:23:37 -0400

    also to the power of four thirds

--- hotkeys.js
+++ hotkeys.js
@@ -43,10 +43,12 @@ async function finishLoadingAudioFile(){const bb=await  loadAudioFile ();
                                     var zb = zoom;
                                     if(zb>.75)zb=.75;
                                    // if(zb<1.)
-                                     uniforms.SEVEYEStart.value=((Math.log(uniforms.baseN.value*(1.-zb))-1.)/2.+1.
+                                     uniforms.SEVEYEStart.value=((Math.log(uniforms.baseN.value*(1.+zb**4./3.))-1.)/2.+1.
                                     )/2.+//((Math.log(3.)-1.)/2.+1.)/2.+Math.log(uniforms.baseN.value))/2.
                                      (fromCenter*(1.+zb**4./3.) 
                                     )**(1./3.);
+                                //    if(!isFinite(uniforms.SEVEYEStart.value)||uniforms.SEVEYEStart.value<0.)
+                               //         console.log(uniforms.SEVEYEStart.value)
                                   //  else uniforms.SEVEYEStart.value=1.;
                                 }
               
