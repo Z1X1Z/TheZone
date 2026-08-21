9ea331f71080aab70693b37eb66a8f641b12b1af
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-21 00:21:13 -0400

    no problem on laptop

--- starshipMod.js
+++ starshipMod.js
@@ -1841,14 +1841,14 @@ if(zoom>2**64)
 
     let zoomCone = metaDepth * fromCenter;
     if (uniforms["colorCombo"].value == 16) zoomCone /= 1.33333333 / 2.;
-    if (zoom >= 1.&&(fromCenter<skyInterSection||zoom>=fromCenter*ZR))//could work as one (also below) but my phone was partially crashing on continuum clover zoomout
+    if (zoom >= 1.&&(fromCenter<skyInterSection||zoom>=fromCenter*.5))//could work as one (also below) but my phone was partially crashing on continuum clover zoomout
         zoomOutEngage = false;
     ZR = setZoomRate();
 
     if (!isFinite(ZR)) ZR = 1;
     if (!zoomAtl41 && !zoomOutEngage && zoomRate > 0.) {
         if ((zoom > zoomCone && ampThresh > zoomOutRatchetThreshold && (on && !window.touchMode)) || xTouch + yTouch != 0) zoom *= ZR ** ((1. + INcreaseBoost) * zoomBoost);
-        else if (uniforms.MetaCored.value || zoom < 1.||(fromCenter>skyInterSection&&zoom<fromCenter*ZR)) {
+        else if (uniforms.MetaCored.value || zoom < 1.||(fromCenter>skyInterSection&&zoom<fromCenter*.5)) {
             zoom /= ZR;
             triggerRailSet = true
             if (center) { coordX *= ZR * 2. / 3.;; coordY *= ZR * 2. / 3.; }
