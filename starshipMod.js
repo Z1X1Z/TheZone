68fec9e9673c4c744738df00c977998ebf9ddd73
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-21 20:21:29 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -4512,7 +4512,7 @@ function constellationCoordFind() {//needs to be tuned for ngenesis
 }
 
 const frameBoosted = 1024
-const wingsOfRighteousness =4.25087423652421874877777769755553;
+const wingsOfRighteousness =4.25087423652421874877777769755559;
 //const wingsOfRighteousness =4.25087423652421874877777769;
 //console.log(wingsOfRighteousness)
 var frameRation = (wingsOfRighteousness +
