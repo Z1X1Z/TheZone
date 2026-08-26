2b822dc1a40e44e2833a4eec3f5e995f4ace66ed
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-25 23:35:28 -0400

    hears bflat better

--- starshipMod.js
+++ starshipMod.js
@@ -4690,8 +4690,11 @@ function calculatePitch() {
             let totalAMPtrunc = (totalAMP/trunc)*trunc;
             let taTRUNC = (tAScaled/trunc)*trunc
             if (tAScaled > 0 && isFinite(tAScaled) && tAScaled < 1.)
+
+                 // for (var reps = 0.; reps < totalAMP ** (7 - totalAMPmodified);//these may be totalAMP or totalAMPmodified
+                //    reps += tAScaledPermanent ** (3. - totalAMP))//reps+=tAScaledPermanent**(3.-tAScaledPermanent))
                 for (var reps = 0.; reps < taTRUNC ** (7 + totalAMPmodified);//these may be totalAMP or totalAMPmodified
-                    reps += taTRUNC ** (3. - totalAMPmodified))//reps+=tAScaledPermanent**(3.-tAScaledPermanent))
+                    reps += taTRUNC ** (3. - totalAMP))//reps+=tAScaledPermanent**(3.-tAScaledPermanent))
                 //   for(var reps=0; reps<2.;reps+=1)
                 //                 for(reps=0;reps<5.;reps+=1.+1./(1.-totalAMPmodified))
 
