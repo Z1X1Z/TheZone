2b9cf93a23f3523b40a8e183bcceadc152bf70f2
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-26 08:20:56 -0400

    added truncator back in

--- starshipMod.js
+++ starshipMod.js
@@ -4895,7 +4895,7 @@ function calculatePitch() {
                 let powerAMP = totalAMP + 1.;
                 tolerance *= ((totalAMP) ** (powerAMP / 3.) + (totalAMP) ** (powerAMP / 2.) + totalAMP ** powerAMP);
 
-                  //     tolerance=((tolerance)/trunc)*trunc
+                      tolerance=((tolerance)/trunc)*trunc//this truncator may need to be removed
 
                 // let nudgeAdjust = totalAMP;
 
