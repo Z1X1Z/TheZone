39e9fdecfe373f33b56725a79760177ead8cdbc5
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-30 02:44:48 -0400

    Add files via upload

--- starshipMod.js
+++ starshipMod.js
@@ -3432,7 +3432,7 @@ function animate(timestamp) {
                         }
 
                         arm = flip * (g * radialWarp + twist * EldersLeg / 24.) % EldersLeg / EldersLeg * pi * 2.;
-                        lengt = ((testar[(g + EldersLeg / 2.) % EldersLeg]) - minTestar) / (maxTestar - minTestar);
+                       if(maxTestar!=minTestar) lengt = ((testar[(g + EldersLeg / 2.) % EldersLeg]) - minTestar) / (maxTestar - minTestar);
                         if (minTestar == testar[(g + EldersLeg / 2.) % EldersLeg] || testar[(g + EldersLeg / 2.) % EldersLeg] < 0.) lengt = 0.;//cull miniumum throughput
                         if (twoOr1) {
                             lengt /= 2. ** 15. / EldersLeg;
