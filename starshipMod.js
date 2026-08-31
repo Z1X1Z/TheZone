33fdeea569e28d902de7636bfdb2b2edf386e00c
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-08-31 08:09:28 -0400

    more efficient and better low F and G

--- starshipMod.js
+++ starshipMod.js
@@ -4809,7 +4809,7 @@ function calculatePitch() {
             //  tolerance=(tolerance/trunc)*trunc
                 var tolStuck = tolerance;
          var metas = 0.
-for(var metaloops = 0.;metaloops<2.+totalAMPtrunc;metaloops+=totalAMPmodified)if(metas<34)
+for(var metaloops = 0.;metaloops<2.+tAScaled;metaloops+=tolerance)if(metas<20)
 {
     metas++
             tolerance = (tolerance + plusOrMinusPowerSeriesBUFFER) ** (.75 + (totalAMPmodified + tAScaledPermanent + tolerance - plusOrMinusPowerSeriesNorm));
@@ -4995,7 +4995,6 @@ for(var metaloops = 0.;metaloops<2.+totalAMPtrunc;metaloops+=totalAMPmodified)if
                                 }
                         }
                                   //                          tolerance *= (adjuster**adjuster)
-
 
                              //    adjuster *= (adjuster**(adjuster**adjuster)**adjuster)**3**powerUP
                                     tolerance *= (adjuster**adjuster)
