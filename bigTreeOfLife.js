e24b654b460f115cb1d803ba784242aec02c7fa9
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-09-13 14:59:18 -0400

    trunc=log(x^(-1/x-1-x))

--- bigTreeOfLife.js
+++ bigTreeOfLife.js
@@ -475,7 +475,7 @@ Math.abs(pABSOLUTE.x)>27.
 &&Math.abs(constellationCoord.x)<27.&&lfc>zoom)//may or may not include 
 {
 // if(!(lengthABSOLUTE<2./3.)) 
- truncNonLeaf=Math.log(Math.pow(lfcCenterCored,-1./lfcCenterCored-1.));
+ truncNonLeaf=Math.log(Math.pow(lfcCenterCored,-1./lfcCenterCored-1.-lfcCenterCored));
 // else  truncNonLeaf=Math.log((lfcCenterCored));
 truncator =truncNonLeaf*-leaf/2.;//*100//log((lfc*lfc)/(zoomB*zoomB))
 
