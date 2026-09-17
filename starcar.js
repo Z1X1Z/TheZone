391ae9e90303ef554b4daeb8a09cf806ca2a7879
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-09-17 00:53:53 -0400

    patch turned to fix

--- starcar.js
+++ starcar.js
@@ -1110,8 +1110,8 @@ function setFFTdependantSizes() {
     bufferSize = fftSize;
     numberOfBins = fftSize / 2.;
     frequencies = new Float64Array(numberOfBins).fill(1);
-    inputData = new Float32Array(bufferSize).fill(1);
-    dataArray = new Uint8Array(numberOfBins).fill(1);
+    inputData = new Float32Array(bufferSize).fill(0);
+    dataArray = new Uint8Array(numberOfBins).fill(0);
     babyBuffer = new Uint8Array(bufferSize).fill(0);
     // window.zoomOutRatchetThreshold=starSHIPVOLUMEdefaultLowVolume;//5./1024;////or 1/1024.//maybe shouldn't need to be here could be solved away
 
