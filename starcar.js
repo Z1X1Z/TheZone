59ed995043ed11a5a9a50ba25a67034d51ae2265
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-09-23 05:15:14 -0400

    totalAMPmodified now has power function

--- starcar.js
+++ starcar.js
@@ -5053,6 +5053,17 @@ function calculatePitch() {
             if (!isFinite(trunc))trunc = 1
             //totalAMPmodified=(totalAMPmodified/((-leafPermanent)/))///preTrunc)*preTrunc));
             totalAMPmodified = (((totalAMPmodified * (-(leafPermanent / trunc) * trunc))))
+            totalAMPmodified=totalAMPmodified**
+            (1.+totalAMPmodified**
+                (1.-totalAMPmodified**
+                    (
+                        ((1.-totalAMPmodified**(1.+totalAMPmodified**totalAMPmodified)
+                        **(totalAMPmodified**totalAMPmodified)))**
+                       ((1.+totalAMPmodified**(1.-totalAMPmodified**totalAMPmodified)
+                       **(totalAMPmodified**totalAMPmodified)))
+                    )**totalAMPmodified
+                ) **(totalAMPmodified**totalAMPmodified)
+          ) 
             //preTrunc = Math.log(totalAMPmodified)*-leafPermanent/2
             //totalAMPmodified=totalAMPmodified**1.5;
             totalAMPmodified = (((totalAMPmodified) / trunc) * trunc)
