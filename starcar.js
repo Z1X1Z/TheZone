e128f7cd106e16e877bfaa3411d63c669cd9d3fc
Z1X1Z <hallelujah.hephzibah@gmail.com>
2026-09-25 15:40:48 -0400

    **(totalAMPmodified**totalAMPmodified)

--- starcar.js
+++ starcar.js
@@ -5072,7 +5072,7 @@ function calculatePitch() {
                         )
                             )**((totalAMPmodified**totalAMPmodified)**totalAMPmodified)
                         ) **((totalAMPmodified)**(totalAMPmodified**totalAMPmodified))  
-                    )  
+                    )  **(totalAMPmodified**totalAMPmodified)
                 )**totalAMPmodified
             //preTrunc = Math.log(totalAMPmodified)*-leafPermanent/2
             //totalAMPmodified=totalAMPmodified**1.5;
